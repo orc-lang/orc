d9c6a137274d4b969f144ce85283cf2597343715
Thomas Finsterbusch <thomas@capehq.com>
2008-02-27 10:28:34 +0000

    Allowing WSDL calls and Java return types 'void'

--- OrcJava/src/orc/runtime/sites/java/MethodProxy.java
+++ OrcJava/src/orc/runtime/sites/java/MethodProxy.java
@@ -16,40 +16,49 @@ import orc.runtime.values.*;
  */
 public class MethodProxy extends EvalSite {
 
-	List<Method> wrapped_methods;
-	Object self;
-	String methodName;
-	
-	public MethodProxy(List<Method> m, Object self, String name)
-	{
-		this.wrapped_methods = m;
-		this.self = self;
-		this.methodName = name;
-	}
-	
-	/* (non-Javadoc)
-	 * @see orc.runtime.sites.EvalSite#evaluate(java.lang.Object[])
-	 */
-	@Override
-	public Value evaluate(Args args) {
-		
-		Object result = null;
-		
-		for (Method m : wrapped_methods)
-		{
-			try
-			{
-				result = m.invoke(self, args.asArray());
-				break;
-			}
-			catch (IllegalArgumentException e) {}
-			catch (Exception e) { throw new Error("Method invocation failure for '" + methodName + "'."); }
-		}
-		
-		if (result == null)
-			{ throw new Error("Argument types did not match any implementation for method '" + methodName + "'."); }
+    List<Method> wrapped_methods;
+    Object self;
+    String methodName;
+    
+    public MethodProxy(List<Method> m, Object self, String name)
+    {
+        this.wrapped_methods = m;
+        this.self = self;
+        this.methodName = name;
+    }
+    
+    /* (non-Javadoc)
+     * @see orc.runtime.sites.EvalSite#evaluate(java.lang.Object[])
+     */
+    @Override
+    public Value evaluate(Args args) {
+        
+        Object result = null;
+        
+        for (Method m : wrapped_methods)
+        {
+            try
+            {
+                result = m.invoke(self, args.asArray());
+                
+                // if return type is void, invoke returns null => create void token
+                if (m.getReturnType().getName().equals("void")) {
+                    result = "void";
+                }
+                
+                break;
+            }
+            catch (IllegalArgumentException e) {}
+            catch (Exception e) { 
+                e.printStackTrace();
+                throw new Error("Method invocation failure for '" + methodName + "'."); 
+            }
+        }
+        
+        if (result == null)
+            { throw new Error("Argument types did not match any implementation for method '" + methodName + "'."); }
 
-		return new Constant(result);
-	}
+        return new Constant(result);
+    }
 
 }
