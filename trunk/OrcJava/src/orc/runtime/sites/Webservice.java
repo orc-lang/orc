ca636a61696053a81fd2922aeef8a84c03b46466
tfinster <tfinster@440195e8-269f-11de-a2b9-47ed1c40409f>
2008-02-27 10:28:34 +0000

    Allowing WSDL calls and Java return types 'void'

--- trunk/OrcJava/src/orc/runtime/sites/Webservice.java
+++ trunk/OrcJava/src/orc/runtime/sites/Webservice.java
@@ -3,128 +3,132 @@ package orc.runtime.sites;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import javax.wsdl.Definition;
-import javax.wsdl.Service;
-import javax.xml.namespace.QName;
 
 import orc.runtime.Args;
-import orc.runtime.OrcEngine;
 import orc.runtime.Token;
-import orc.runtime.sites.java.MethodProxy;
 import orc.runtime.sites.java.ObjectProxy;
-import orc.runtime.values.GroupCell;
-import orc.runtime.values.Tuple;
 import orc.runtime.values.Value;
 
-import org.apache.axis.description.ServiceDesc;
-import org.apache.axis.encoding.TypeMapping;
 import org.apache.axis.wsdl.toJava.Emitter;
 import org.apache.axis.wsdl.toJava.GeneratedFileInfo;
 import org.apache.axis.wsdl.toJava.GeneratedFileInfo.Entry;
 
 public class Webservice extends EvalSite {
 
-	// Emitter is the class that does all of the file creation for the WSDL2Java
-	// tool.  Using Emitter will allow us to get immediate access to the class names that it
-	// generates
-	
-	private Emitter emitter;
+    // Emitter is the class that does all of the file creation for the WSDL2Java
+    // tool.  Using Emitter will allow us to get immediate access to the class names that it
+    // generates
+    private Emitter emitter;
+    private Class locator;
+    private Class stub;
 
-	private Class locator;
+    ObjectProxy op;
 
-	private Class stub;
+    // create the java code and get info about that code
+    public GeneratedFileInfo createJavaCode(String url) throws Exception {
+        emitter = new Emitter();
+        emitter.setOutputDir("./webservices/");
+        emitter.run(url);
+        
+        return emitter.getGeneratedFileInfo();
+    }
 
-	ObjectProxy op;
+    // compile all of the class files in real-time
+    public void compileJavaCode(GeneratedFileInfo info) {
+        List<String> fileNames = (ArrayList<String>) info.getFileNames();
+        
+        for (String file : fileNames) {
+            com.sun.tools.javac.Main
+                    .compile(new String[] {
+                            "-cp",
+                            "./webservices:./lib/axis.jar:./lib/jaxrpc.jar",
+                            file });
+        }
+        
+        
+    }
 
-	// create the java code and get info about that code
-	public GeneratedFileInfo createJavaCode(String url) throws Exception {
-		emitter = new Emitter();
-		emitter.setOutputDir("./webservices/");
-		emitter.run(url);
-		
-		return emitter.getGeneratedFileInfo();
-	}
+    @Override
+    public void callSite(Args args, Token caller) {
 
-	// compile all of the class files in real-time
-	public void compileJavaCode(GeneratedFileInfo info) {
-		List<String> fileNames = (ArrayList<String>) info.getFileNames();
-		
-		for (String file : fileNames) {
-			com.sun.tools.javac.Main
-					.compile(new String[] {
-							"-cp",
-							"./webservices:./lib/axis.jar:./lib/jaxrpc.jar",
-							file });
-		}
-		
-		
-	}
+        try {
+            // take the passed URL and create java code from it
+            GeneratedFileInfo info = createJavaCode(args.stringArg(0));
 
-	@Override
-	public void callSite(Args args, Token caller) {
+            // compile that java code
+            compileJavaCode(info);
+            
 
-		try {
-			// take the passed URL and create java code from it
-			GeneratedFileInfo info = createJavaCode(args.stringArg(0));
+            List<Entry> stubs = (ArrayList<Entry>) info.findType("interface");
 
-			// compile that java code
-			compileJavaCode(info);
-			
+            
+            for(Entry e : stubs){
+                Class c = Class.forName(e.className);
+                if(c.getName().endsWith("Port") || c.getName().endsWith("PortType")) {
+                    stub = Class.forName(e.className);
+                    break;
+                }
+                for (Class iface : c.getInterfaces()) {
+                    if (iface.getName().equals("java.rmi.Remote")) {
+                        stub = Class.forName(e.className);
+                        break;
+                    }
+                }
+            }
+            
+            if (stub == null) {
+                throw new Error("Unable to find stub among port interfaces");
+            }           
 
-			List<Entry> stubs = (ArrayList<Entry>) info.findType("interface");
+            List<Entry> services = (ArrayList<Entry>) info.findType("service");
+            
+            for(Entry e : services){
+                if(e.className.endsWith("Locator")){
+                    locator = Class.forName(e.className);
+                }
+            }
+            
+            if (locator == null) {
+                throw new Error("Unable to find Locator among services");
+            }
 
-			for(Entry e : stubs){
-				if(e.className.endsWith("PortType")){
-					stub = Class.forName(e.className);
-				}
-			}
-			
+            
+            Method[] locatorMethods = locator.getMethods();
+            Method getStub = null;
 
-			List<Entry> services = (ArrayList<Entry>) info.findType("service");
-			
-			for(Entry e : services){
-				if(e.className.endsWith("Locator")){
-					locator = Class.forName(e.className);
-				}
-			}
+            //use the stub with the default no-arg constructor
+            for (int i = 0; i < locatorMethods.length; i++) {
+                if (locatorMethods[i].getReturnType().equals(stub)
+                        && locatorMethods[i].getParameterTypes().length == 0) {
+                    getStub = locatorMethods[i];
+                }
+            }
 
-			
-			Method[] locatorMethods = locator.getMethods();
-			Method getStub = null;
+            if (getStub == null) {
+                throw new Error("Unable to find getStub method within Locator");
+            }
 
-			//use the stub with the default no-arg constructor
-			for (int i = 0; i < locatorMethods.length; i++) {
-				if (locatorMethods[i].getReturnType().equals(stub)
-						&& locatorMethods[i].getParameterTypes().length == 0) {
-					getStub = locatorMethods[i];
-				}
-			}
+            Object arglist[] = new Object[0];
+            Object locatorObject = locator.newInstance();
+            Object stubObject = getStub.invoke(locatorObject, arglist);
 
-			
+            op = new ObjectProxy(stubObject);
+            
+            caller.resume(evaluate(args));
+            
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-			Object arglist[] = new Object[0];
-			Object locatorObject = locator.newInstance();
-			Object stubObject = getStub.invoke(locatorObject, arglist);
+    }
 
-			op = new ObjectProxy(stubObject);
-			
-			caller.resume(evaluate(args));
-			
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	@Override
-	public Value evaluate(Args args) {
-		return op;
-	}
+    @Override
+    public Value evaluate(Args args) {
+        return op;
+    }
 
 }
