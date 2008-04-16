0ebf894c7725f3be315451ce6ab27e973dad3e47
David Kitchin <dkitchin@gmail.com>
2008-04-16 22:52:57 +0000

    Simplified list creation as a static method in ListValue

--- OrcJava/src/orc/runtime/values/ListValue.java
+++ OrcJava/src/orc/runtime/values/ListValue.java
@@ -8,4 +8,14 @@ public abstract class ListValue extends Value {
 	
 	public abstract List<Value> enlist();
 	
+	public static ListValue make(List<Value> vs) {
+		
+		ListValue l = new NilValue();
+		
+		for(int i = vs.size() - 1; i >= 0; i--) {
+			l = new ConsValue(vs.get(i), l);
+		}
+		
+		return l;
+	}
 }
