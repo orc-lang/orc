2b763304fae1be230fe35eea1cbaacf22e4cb90d
adrianquark <adrianquark@440195e8-269f-11de-a2b9-47ed1c40409f>
2008-07-22 19:05:48 +0000

    Simplify continuation usage slightly by providing a withToken method which runs a computation in the context of a token.

--- trunk/OrcJava/src/orc/runtime/sites/SimpleSite.java
+++ trunk/OrcJava/src/orc/runtime/sites/SimpleSite.java
@@ -3,6 +3,8 @@ package orc.runtime.sites;
 import orc.error.JavaException;
 import orc.error.TokenException;
 import orc.runtime.Args;
+import orc.runtime.Continuation;
+import orc.runtime.Continuation.Thunk;
 import orc.runtime.Token;
 import orc.runtime.values.Value;
 
@@ -15,20 +17,12 @@ import orc.runtime.values.Value;
  */
 public abstract class SimpleSite extends Site {
 	@Override
-	public void callSite(Args args, Token caller) throws TokenException {
-		caller.getEngine().setCurrentToken(caller);
-		Value value = evaluate(args);
-		// If evaluation throws an exception, either before or after calling
-		// suspend, it will be handled externally by killing the token. If the
-		// site managed to save the token before throwing the exception, that's
-		// ok, the token won't allow itself to be processed if it is ever
-		// resumed.
-		caller = caller.getEngine().getCurrentToken();
-		if (caller != null) {
-			// the site never suspended, so we can go ahead
-			// and resume with the value it returned
-			caller.resume(value);
-		}
+	public void callSite(final Args args, Token caller) throws TokenException {
+		Continuation.withToken(caller, new Thunk() {
+			public Value apply() throws TokenException {
+				return evaluate(args);
+			}
+		});
 	}
 	/**
 	 * If this method calls Continuation.suspend(), its return value will be
