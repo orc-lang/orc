77bd8067cdb9be038e46136113306fa89e52dd59
adrianquark <adrianquark@440195e8-269f-11de-a2b9-47ed1c40409f>
2008-07-22 22:37:28 +0000

    Experimental Google Search which uses lazy lists. I haven't been able to test it yet.

--- branches/quark/OrcSites/src/orc/lib/net/GoogleSearch.java
+++ branches/quark/OrcSites/src/orc/lib/net/GoogleSearch.java
@@ -8,15 +8,17 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
-import orc.error.JavaException;
 import orc.error.OrcError;
 import orc.error.TokenException;
 import orc.runtime.Args;
-import orc.runtime.sites.EvalSite;
+import orc.runtime.Continuation;
+import orc.runtime.Thunk;
 import orc.runtime.sites.ThreadedSite;
-import orc.runtime.values.ConsValue;
-import orc.runtime.values.ListValue;
-import orc.runtime.values.NilValue;
+import orc.runtime.values.LazyListValue;
+import orc.runtime.values.NoneValue;
+import orc.runtime.values.OptionValue;
+import orc.runtime.values.SomeValue;
+import orc.runtime.values.TupleValue;
 import orc.runtime.values.Value;
 
 import org.codehaus.jettison.json.JSONArray;
@@ -27,8 +29,7 @@ import org.codehaus.jettison.json.JSONObject;
  * Wrapper for the Google Search AJAX API described at
  * http://code.google.com/apis/ajaxsearch/documentation/#fonje
  * 
- * Returns a list of pages, where each page is a site which
- * returns a list of result GSearch objects. See the Google Search
+ * Returns a list of GSearch result objects. See the Google Search
  * AJAX API documentation for details. Properties include:
  * <ul>
  * <li>unescapedUrl
@@ -49,70 +50,84 @@ public class GoogleSearch extends ThreadedSite {
 	/** FIXME: this should be configurable, i.e. via properties file */
 	private final static String httpReferer = "http://orc.csres.utexas.edu/orchard";
 	
+	public static class ResultsThunk implements Thunk<OptionValue<TupleValue>> {
+		private String url;
+		private int page;
+		private JSONArray results;
+		private int maxPages;
+		private int index;
+		public ResultsThunk(String url) {
+			this(url, 0);
+		}
+		public ResultsThunk(String url, int page) {
+			this(url, page, null, 0, 0);
+		}
+		private ResultsThunk(String url, int page, JSONArray results, int index, int maxPages) {
+			this.url = url;
+			this.page = page;
+			this.results = results;
+			this.index = index;
+			this.maxPages = maxPages;
+		}
+		public OptionValue<TupleValue> apply() {
+			if (results != null) {
+				// We have results already;
+				// skip to the next available one
+    			while (index < results.length()) {
+    				try {
+    					Object result = results.get(index);
+    					ResultsThunk next = new ResultsThunk(
+    							url, page, results, index+1, maxPages);
+    					// return the current result
+        				return new SomeValue<TupleValue>(new TupleValue(
+        						JSONSite.wrapJSON(result), new LazyListValue(next)));
+    				} catch (JSONException e) {
+        				index++;
+    				}
+    			}
+    			// If no more are available, go to the next page
+    			if (page+1 >= maxPages) {
+    				return new NoneValue<TupleValue>();
+    			} else {
+        			return new ResultsThunk(url, page+1).apply();
+    			}
+			} else {
+				// We don't have any results yet
+    			final Continuation c = Continuation.suspend();
+    			new Thread() {
+    				public void run() {
+            			try {
+            				JSONObject root = requestJSON(new URL(url + "&start=" + page));
+                			JSONObject response = root.getJSONObject("responseData");
+                			results = response.getJSONArray("results");
+                			maxPages = response.getJSONObject("cursor").getJSONArray("pages").length();
+                			c.resume(apply());
+            			} catch (MalformedURLException e) {
+            				c.error(e);
+            			} catch (IOException e) {
+            				c.error(e);
+            			} catch (JSONException e) {
+            				c.error(e);
+            			}
+    				}
+    			}.start();
+    			return null;
+			}
+		}
+	}
+	
 	@Override
 	public Value evaluate(Args args) throws TokenException {
-		final String url;
-		final JSONArray results;
-		final JSONArray pages;
-		// get the first page of results and the cursor
 		try {
 			String search = args.stringArg(0);
-			url = apiURL + "?v=1.0" +
+			String url = apiURL + "?v=1.0" +
 					"&q=" + URLEncoder.encode(search, "UTF-8") +
 					"&key=" + apiKey;
-			JSONObject root = requestJSON(new URL(url));
-			JSONObject response = root.getJSONObject("responseData");
-			results = response.getJSONArray("results");
-			pages = response.getJSONObject("cursor").getJSONArray("pages");
+			return new LazyListValue(new ResultsThunk(url, 0));
 		} catch (UnsupportedEncodingException e) {
 			// should be impossible
 			throw new OrcError(e);
-		} catch (MalformedURLException e) {
-			// should be impossible
-			throw new OrcError(e);
-		} catch (IOException e) {
-			throw new JavaException(e);
-		} catch (JSONException e) {
-			throw new JavaException(e);
 		}
-		
-		// build a list of pages
-		ListValue out = new NilValue();
-		for (int i = pages.length()-1; i > 1; --i) {
-			final String page;
-			try {
-				page = pages.getJSONObject(i).getString("start");
-				out = new ConsValue(new ThreadedSite() {
-					@Override
-					public Value evaluate(Args args) throws TokenException {
-						JSONObject root;
-						try {
-							root = requestJSON(new URL(url + "&start=" + page));
-							JSONObject response = root.getJSONObject("responseData");
-							JSONArray results = response.getJSONArray("results");
-							return JSONSite.wrapJSON(results);
-						} catch (MalformedURLException e) {
-							// should be impossible
-							throw new OrcError(e);
-						} catch (IOException e) {
-							throw new JavaException(e);
-						} catch (JSONException e) {
-							throw new JavaException(e);
-						}
-					}
-				}, out);
-			} catch (JSONException e) {
-				// Skip bad or missing cursors
-			}
-		}
-		// the first page, we already got the results, so it is much simpler
-		out = new ConsValue(new EvalSite() {
-			@Override
-			public Value evaluate(Args args) throws TokenException {
-				return JSONSite.wrapJSON(results);
-			}
-		}, out);
-		return out;
 	}
 	
 	/** Utility method to make a JSON request. */
