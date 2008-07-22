44919171eb85eaa9f2aa98a6eb68dc2f0e6a3a2f
adrianquark <adrianquark@440195e8-269f-11de-a2b9-47ed1c40409f>
2008-07-22 03:53:07 +0000

    New GoogleSearch and Ping sites.

---
+++ trunk/OrcSites/src/orc/lib/net/JSONSite.java
@@ -0,0 +1,66 @@
+package orc.lib.net;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import orc.error.JavaException;
+import orc.error.SiteException;
+import orc.error.TokenException;
+import orc.runtime.Args;
+import orc.runtime.sites.EvalSite;
+import orc.runtime.values.Constant;
+import orc.runtime.values.ListValue;
+import orc.runtime.values.Value;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+/**
+ * Wrap a JSONObject to create an Orc site. Properties
+ * may be accessed with dotted syntax. Array values are
+ * turned into Orc lists, object values are turned into
+ * sites, and all other values are turned into the Orc
+ * atomic types you would expect.
+ * 
+ * @author quark
+ */
+class JSONSite extends EvalSite {
+	private JSONObject root;
+	private JSONSite(JSONObject root) {
+		this.root = root;
+	}
+	public static Value wrapJSON(Object o) {
+		if (o instanceof JSONObject) {
+			return new JSONSite((JSONObject)o); 
+		} else if (o instanceof JSONArray) {
+			List<Value> out = new LinkedList<Value>();
+			JSONArray a = (JSONArray)o;
+			for (int i = 0; i < a.length(); ++i) {
+				Object e;
+				try {
+					e = a.get(i);
+				} catch (JSONException _) {
+					// the index does not exist
+					e = null;
+				}
+				out.add(wrapJSON(e));
+			}
+			return ListValue.make(out);
+		} else {
+			return new Constant(o);
+		}
+	}
+	@Override
+	public Value evaluate(Args args) throws TokenException {
+		try {
+			if (root == null) {
+				throw new SiteException("Tried to get a property from" +
+						" a null JSON object.");
+			}
+			return wrapJSON(root.get(args.fieldName()));
+		} catch (JSONException e) {
+			throw new JavaException(e);
+		}
+	}
+}
