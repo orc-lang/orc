8abcdb2e2586f38b49810cc63ba44a82442bd4dd
Adrian Quark <adrian@sixfingeredman.net>
2008-07-23 21:56:42 +0000

    Wrapper around OAuth which should make OAuth-based sites easier to develop.

---
+++ OrcSites/src/com/centerkey/utils/BareBonesBrowserLaunch.java
@@ -0,0 +1,57 @@
+package com.centerkey.utils;
+
+import java.lang.reflect.Method;
+import javax.swing.JOptionPane;
+
+/**
+ * Bare Bones Browser Launch for Java<br>
+ * Utility class to open a web page from a Swing application
+ * in the user's default browser.<br>
+ * Supports: Mac OS X, GNU/Linux, Unix, Windows XP<br>
+ * Example Usage:<code><br> &nbsp; &nbsp;
+ *    String url = "http://www.google.com/";<br> &nbsp; &nbsp;
+ *    BareBonesBrowserLaunch.openURL(url);<br></code>
+ * Latest Version: <a href="http://www.centerkey.com/java/browser/">http://www.centerkey.com/java/browser</a><br>
+ * Author: Dem Pilafian<br>
+ * Public Domain Software -- Free to Use as You Like
+ * @version 1.5, December 10, 2005
+ */
+public class BareBonesBrowserLaunch {
+
+   private static final String errMsg = "Error attempting to launch web browser";
+
+   /**
+    * Opens the specified web page in a web browser
+    * @param url  An absolute URL of a web page (ex: "http://www.google.com/")
+    */
+   public static void openURL(String url) {
+      String osName = System.getProperty("os.name");
+      try {
+         if (osName.startsWith("Mac OS")) {
+            Class fileMgr = Class.forName("com.apple.eio.FileManager");
+            Method openURL = fileMgr.getDeclaredMethod("openURL",
+               new Class[] {String.class});
+            openURL.invoke(null, new Object[] {url});
+            }
+         else if (osName.startsWith("Windows"))
+            Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+         else { //assume Unix or Linux
+            String[] browsers = {
+            	"x-www-browser", "firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
+            String browser = null;
+            for (int count = 0; count < browsers.length && browser == null; count++)
+               if (Runtime.getRuntime().exec(
+                     new String[] {"which", browsers[count]}).waitFor() == 0)
+                  browser = browsers[count];
+            if (browser == null)
+               throw new Exception("Could not find web browser");
+            else
+               Runtime.getRuntime().exec(new String[] {browser, url});
+            }
+         }
+      catch (Exception e) {
+         JOptionPane.showMessageDialog(null, errMsg + ":\n" + e.getLocalizedMessage());
+         }
+      }
+
+   }
