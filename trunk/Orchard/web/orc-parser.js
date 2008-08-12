d49bbcb04ecb104de0798692d2f9e7768f639fe2
adrianquark <adrianquark@440195e8-269f-11de-a2b9-47ed1c40409f>
2008-08-12 18:54:06 +0000

    Web UI syntax highlighter now accepts < and > as operators (and "stop" as "null"). To avoid doing a deep parse, it uses significant whitespace to guess whether you are starting a combinator or using an operator.

--- trunk/Orchard/web/orc-parser.js
+++ trunk/Orchard/web/orc-parser.js
@@ -1,5 +1,15 @@
 /**
- * Parser for Orc.
+ * Syntax-highlighting parser for Orc.
+ * - This depends on the CodeMirror library: http://marijn.haverbeke.nl/codemirror
+ * - Recognizes the following syntactic entities
+ *   - combinator, pattern, site, variable, keyword, literal, comment
+ * - The actual parsing is very shallow, mostly we just tokenize
+ * - The combinator "<_<" is distinguished from the operator "<" using
+ *   singificant whitespace (unlike the real Orc parser). A combinator
+ *   should be preceeded by whitespace and followed by non-whitespace.
+ * - sites are distinguished from variables based on whether they
+ *   are followed by a "(".
+ * - The actual styles are defined in orc-syntax.css
  */
 Editor.Parser = (function() {
 
@@ -18,8 +28,10 @@ var ops2 = {
 // 1-character symbolic tokens
 var ops1 = {
 "{": readOperator,
-"<": readCombinator,
-">": readCombinator,
+// the parser will decide whether this
+// is an operator or not
+"<": readOperator,
+">": readOperator,
 "|": readCombinator,
 ":": readOperator,
 "&": readOperator,
@@ -46,6 +58,12 @@ var ops1 = {
 var isWord = matcher(/[\w_0-9]/);
 var isDigit = matcher(/[0-9]/);
 
+/**
+ * Custom tokenizer. This always separates whitespace and
+ * non-whitespace tokens so we can get more accurate information
+ * for indentation. We may have to rethink that if it makes the parser
+ * inefficient.
+ */
 function tokenizer(source, state) {
 	function isSpace(ch) {
 		// The messy regexp is because IE's regexp matcher is of the
@@ -132,7 +150,7 @@ function readWord(source, _, ch1) {
 		return { type:"boolean", content:word, style:"literal" };
 	// keywords
 	case "val": case "def": case "as": case "include":
-	case "site": case "class": case "null":
+	case "site": case "class": case "null": case "stop":
 		return { type:word, content:word, style:"keyword" };
 	default:
 		return { type:"variable", content:word, style:"variable" };
@@ -172,21 +190,37 @@ function readCommentSL(source, setState, _) {
 }
 
 /**
- * Constructor for a Parser object.
+ * Orc parser factory. A codemirror parser is just a token stream
+ * with a copy method that is used to cache the parser state at the
+ * start of each line.
  */
 function newParser(source) {
 	var tokens = tokenizer(source, readToken);
+	// buffer for look-ahead tokens
 	var lookahead = [];
+	// while looking, where are we in the lookahead buffer?
 	var lookaheadIndex = 0;
+	// the current tabstop column
 	var tabstop = null;
+	// are we inside a pattern?
+	var inPattern = false;
+	// are we immediately preceeded by whitspace?
+	var inWhitespace = false;
+	/** Reset the lookahead pointer to the current token. */
 	function startLooking() {
 		lookaheadIndex = 0;
 	}
+	/** Get the next token for real and move the "current" token pointer. */
 	function nextToken() {
 		startLooking();
 		if (lookahead.length) return lookahead.shift();
 		else return tokens.next();
 	}
+	/**
+	 * Get the next token speculatively. Repeated calls
+	 * will move forward in the token stream. May be reset
+	 * to the "current" token using startLooking().
+	 */
 	function look() {
 		try {
 			if (lookahead.length > lookaheadIndex) {
@@ -197,6 +231,7 @@ function newParser(source) {
 			}
 		} catch (e) {}
 	}
+	/** Get the next token and change its style based on the parse state. */
 	function next() {
 		var out = nextToken();
 		if (out.content == "\n") {
@@ -207,9 +242,10 @@ function newParser(source) {
 			};
 			tabstop = null;
 		} else {
+			// if the variable is in function call position,
+			// change it to a "site"
 			if (out.type == "variable") {
 				startLooking();
-				// check if the variable should be a function
 				do {
 					var tmp = look();
 					if (!tmp) break;
@@ -219,6 +255,28 @@ function newParser(source) {
 					}
 				} while (tmp.type == "whitespace");
 			}
+			// if we're in a pattern, apply the "pattern" style
+			if (inPattern) out.style += " pattern";
+			// decide whether < or > is an operator or combinator
+			if (out.type == "<" || out.type == ">") {
+				if (inPattern) {
+					// < and > never appear inside a pattern,
+					// so they must be terminating the combinator
+					out.style = "combinator";
+					inPattern = false;
+				} else if (inWhitespace) {
+					// if it's preceeded by space and followed
+					// by non-space, it's a combinator
+					startLooking();
+					var tmp = look();
+					if (tmp && tmp.type != "whitespace") {
+						out.style = "combinator";
+						inPattern = true;
+					}
+				}
+			}
+			// if no tabstop has been set yet, use the
+			// length of the first whitespace token
 			if (tabstop == null) {
 				if (out.type == "whitespace") {
 					tabstop = out.value.length;
@@ -227,15 +285,22 @@ function newParser(source) {
 				}
 			}
 		}
+		inWhitespace = (out.type == "whitespace");
 		return out;
 	}
 	function copy() {
 		var _state = tokens.state;
+		var _inPattern = inPattern;
 		return function (source) {
 			tokens = tokenizer(source, _state);
+			inPattern = _inPattern;
+			// the copy starts at the beginning
+			// of the line, so most of the within-line
+			// state should just be reset
 			lookahead = [];
 			lookaheadIndex = 0;
 			tabstop = null;
+			inWhitespace = true;
 			return parser;
 		}
 	}
