9670e5ed2da280b7503403ba3dc5b5be2f56f689
Adrian Quark <adrian@sixfingeredman.net>
2008-08-28 14:33:11 +0000

    Debugger is working at a very primitive level. Time to start working on UI improvements.

--- Ford/src/orc/ford/Debugger.java
+++ Ford/src/orc/ford/Debugger.java
@@ -2,23 +2,54 @@ package orc.ford;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 import orc.error.Located;
 import orc.error.SourceLocation;
+import orc.trace.BackwardEventCursor;
 import orc.trace.EventCursor;
+import orc.trace.InputStreamEventCursor;
 import orc.trace.EventCursor.EndOfStream;
-import orc.trace.events.*;
+import orc.trace.events.AfterEvent;
+import orc.trace.events.BeforeEvent;
+import orc.trace.events.BlockEvent;
+import orc.trace.events.ChokeEvent;
+import orc.trace.events.DieEvent;
+import orc.trace.events.ErrorEvent;
+import orc.trace.events.Event;
+import orc.trace.events.ForkEvent;
+import orc.trace.events.FreeEvent;
+import orc.trace.events.PrintEvent;
+import orc.trace.events.PublishEvent;
+import orc.trace.events.PullEvent;
+import orc.trace.events.ReceiveEvent;
+import orc.trace.events.SendEvent;
+import orc.trace.events.StoreEvent;
+import orc.trace.events.UnblockEvent;
+import orc.trace.events.Visitor;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
 public class Debugger {
+	/**
+	 * Track the current state of a thread in the debugger.
+	 * Currently this is just the last event seen by the thread.
+	 */
 	private static class ThreadState implements Located {
 		public Event lastEvent;
 		public ThreadState(Event lastEvent) {
@@ -30,17 +61,29 @@ public class Debugger {
 		public boolean isDead() {
 			return lastEvent instanceof DieEvent;
 		}
-		public boolean isBlocked() {
-			return lastEvent instanceof BlockEvent;
-		}
 	}
 	
+	/**
+	 * Cached source file.
+	 */
 	private interface SourceFile {
+		/**
+		 * Print a location in a source file; this means the lines covered by
+		 * the location and some markers to delimit the extent of the location.
+		 */
 		public void printLocation(Writer out, SourceLocation location) throws IOException;
+		public void printShortLocation(Writer out, SourceLocation location) throws IOException;
 	}
 	
+	/**
+	 * Used if the source file cannot be loaded.
+	 */
 	private static class UnavailableSourceFile implements SourceFile {
 		public void printLocation(Writer out, SourceLocation location) throws IOException {
+			out.write(location.toString());
+			out.write("\n");
+		}
+		public void printShortLocation(Writer out, SourceLocation location) throws IOException {
 			out.write(location.toString());
 			out.write("\n");
 		}
@@ -60,48 +103,103 @@ public class Debugger {
 		public void printLocation(Writer out, SourceLocation location) throws IOException {
 			out.write(location.toString());
 			out.write("\n");
-			if (location == SourceLocation.UNKNOWN) return;
-			if (location.line == location.endLine) {
-				printMarkedLine(out, location.line, location.column, location.endColumn);
+			int startL = location.line;
+			int endL = location.endLine;
+			int startC = location.column;
+			int endC = location.endColumn;
+			if (startL == endL) {
+				String text = lines.get(startL-1);
+				out.write(text.substring(0, startC-1));
+				out.write("{");
+				out.write(text.substring(startC-1, endC-1));
+				out.write("}");
+				out.write(text.substring(endC-1));
 			} else {
-				printMarkedLine(out, location.line, location.column, 0);
-				for (int line = location.line+1; line < location.endLine; ++line) {
-					out.write(lines.get(line-1));
+				String text;
+				text = lines.get(startL-1);
+				out.write(text.substring(0, startC-1));
+				out.write("{");
+				out.write(text.substring(startC-1));
+				out.write("\n");
+				for (int line = startL+1; line < endL; ++line) {
+					text = lines.get(line-1);
+					out.write(text);
 					out.write("\n");
 				}
-				printMarkedLine(out, location.line, 0, location.endColumn);
+				if (endL > lines.size()) {
+					// not sure how this happens, I guess the parser
+					// adds a newline at the end of the file which may
+					// or may not be actually present
+					out.write("}");
+				} else {
+					text = lines.get(endL-1);
+					out.write(text.substring(0, endC-1));
+					out.write("}");
+					out.write(text.substring(endC-1));
+				}
 			}
+			out.write("\n");
 		}
-		private void printMarkedLine(Writer out, int line, int start, int end) throws IOException {
-			assert(start != 0 || end != 0);
-			String text = lines.get(line-1);
-			if (start != 0) {
-				out.write(text.substring(0, start-1));
+		public void printShortLocation(Writer out, SourceLocation location) throws IOException {
+			out.write(location.toString());
+			out.write(": ");
+			int startL = location.line;
+			int endL = location.endLine;
+			int startC = location.column;
+			int endC = location.endColumn;
+			if (startL == endL) {
+				String text = lines.get(startL-1);
+				out.write(text.substring(0, startC-1));
 				out.write("{");
-			}
-			if (end != 0) {
-				out.write(text.substring(start, end-1));
+				out.write(text.substring(startC-1, endC-1));
 				out.write("}");
-				out.write(text.substring(end));
+				out.write(text.substring(endC-1));
 			} else {
-				out.write(text.substring(start));
+				String text;
+				text = lines.get(startL-1);
+				out.write(text.substring(0, startC-1));
+				out.write("{...}");
+				if (endL > lines.size()) {
+					// not sure how this happens, I guess the parser
+					// adds a newline at the end of the file which may
+					// or may not be actually present
+				} else {
+					text = lines.get(endL-1);
+					out.write(text.substring(endC-1));
+				}
 			}
 			out.write("\n");
 		}
 	}
 	
-	private Map<ForkEvent, ThreadState> threadStates = new TreeMap<ForkEvent, ThreadState>(); 
+	/** Thread states indexed by thread event */
+	private Map<ForkEvent, ThreadState> threadStates = new HashMap<ForkEvent, ThreadState>(); 
+	/** List of currently debugged threads. The "primary" thread is the first in the list. */
 	private List<ForkEvent> threads = new LinkedList<ForkEvent>();
+	/** The current event cursor. */
 	private EventCursor cursor;
-	private Map<File, SourceFile> sourceFiles = new TreeMap<File, SourceFile>();
+	/** Cached source files. */
+	private Map<File, SourceFile> sourceFiles = new HashMap<File, SourceFile>();
 	private Simulator FORWARD = new ForwardSimulator();
 	private Simulator BACKWARD = new BackwardSimulator();
 	
-	public Debugger(EventCursor cursor) {
-		Event current = cursor.current();
-		this.cursor = cursor;
-		addThread(current.getThread(), current);
-		simulate(current, FORWARD);
+	public Debugger() {}
+	
+	@Argument(metaVar="file", required=true, usage="Input file. Omit to use STDIN.")
+	public void setInputFile(File file) throws CmdLineException {
+		try {
+			cursor = new BackwardEventCursor(
+					new InputStreamEventCursor(
+							new FileInputStream(file)));
+			Event current = cursor.current();
+			addThread((ForkEvent)current, current);
+		} catch (FileNotFoundException e) {
+			throw new CmdLineException("Could not find input file '"+file+"'");
+		} catch (IOException e) {
+			throw new CmdLineException(e);
+		} catch (ClassCastException e) {
+			throw new CmdLineException("That trace file is missing the expected RootEvent");
+		}
 	}
 	
 	private SourceFile getSourceFile(File file) {
@@ -117,7 +215,21 @@ public class Debugger {
 	}
 	
 	private void printLocation(Writer out, SourceLocation location) throws IOException {
-		getSourceFile(location.file).printLocation(out, location);
+		if (location.file == null) {
+			out.write(location.toString());
+			out.write("\n");
+		} else {
+			getSourceFile(location.file).printLocation(out, location);
+		}
+	}
+	
+	private void printShortLocation(Writer out, SourceLocation location) throws IOException {
+		if (location.file == null) {
+			out.write(location.toString());
+			out.write("\n");
+		} else {
+			getSourceFile(location.file).printShortLocation(out, location);
+		}
 	}
 	
 	private ThreadState getThreadState(ForkEvent thread) {
@@ -125,34 +237,62 @@ public class Debugger {
 	}
 	
 	private void addThread(ForkEvent thread, Event event) {
+		if (threadStates.containsKey(event)) return;
 		threadStates.put(thread, new ThreadState(event));
 		threads.add(thread);
 	}
 	
+	protected void printPrimaryThreadState(Writer out) throws IOException {
+		printLocation(out, cursor.current().getSourceLocation());
+		cursor.current().prettyPrint(out, 0);
+		out.write("\n");
+	}
+	
 	protected void printThreadState(Writer out, ThreadState state) throws IOException {
-		printLocation(out, state.getSourceLocation());
+		printShortLocation(out, state.getSourceLocation());
 		state.lastEvent.prettyPrint(out, 0);
+		out.write("\n");
+	}
+	
+	public void print() throws IOException {
+		Writer out = new OutputStreamWriter(System.out);
+		String sep = "-------------------------------------------------\n";
+		Iterator<ForkEvent> threadsi = threads.iterator();
+		out.write(threadsi.next().toString());
+		out.write(": ");
+		printPrimaryThreadState(out);
+		int i = 1;
+		while (threadsi.hasNext()) {
+			ForkEvent thread = threadsi.next();
+			out.write(sep);
+			out.write(Integer.toString(i) + ". ");
+			out.write(thread.toString());
+			out.write(": ");
+			printThreadState(out, getThreadState(thread));
+			++i;
+		}
+		out.write(sep);
+		out.flush();
 	}
 	
 	public void forward() throws EndOfStream {
-		Event current;
 		// cannot move a dead thread forward
-		if (getThreadState(threads.get(0)).isDead()) {
+		if (cursor.current().getThread() == threads.get(0)
+				&& cursor.current() instanceof DieEvent) {
 			throw new EndOfStream();
 		}
 		removeDeadThreads();
 		do {
+			simulate(cursor.current(), FORWARD);
 			cursor = cursor.forward();
-			current = cursor.current();
-		} while (simulate(current, FORWARD));
+		} while (isBoring(cursor.current()));
 	}
 	
 	public void backward() throws EndOfStream {
-		Event current;
 		do {
 			cursor = cursor.backward();
-			current = cursor.current();
-		} while (simulate(current, BACKWARD));
+			simulate(cursor.current(), BACKWARD);
+		} while (isBoring(cursor.current()));
 	}
 	
 	public void removeThread(int index) {
@@ -195,92 +335,164 @@ public class Debugger {
 		}
 	}
 	
-	private abstract class Simulator implements Visitor<Boolean> {
-		public Boolean visit(AfterEvent event) {
-			return false;
-		}
-
-		public Boolean visit(BeforeEvent event) {
-			return true;
-		}
-
-		public Boolean visit(BlockEvent event) {
-			return false;
-		}
-
-		public Boolean visit(ChokeEvent event) {
-			return false;
-		}
-
-		public Boolean visit(DieEvent event) {
-			return false;
-		}
-
-		public Boolean visit(ErrorEvent event) {
-			return false;
-		}
-
-		public Boolean visit(FreeEvent event) {
-			return true;
-		}
-
-		public Boolean visit(PrintEvent event) {
-			return false;
-		}
-
-		public Boolean visit(PublishEvent event) {
-			return false;
-		}
-
-		public Boolean visit(PullEvent event) {
-			return true;
-		}
-
-		public Boolean visit(ReceiveEvent event) {
-			return false;
-		}
-
-		public Boolean visit(RootEvent event) {
-			throw new AssertionError("Unexpected RootEvent");
-		}
-
-		public Boolean visit(SendEvent event) {
-			return false;
-		}
-
-		public Boolean visit(StoreEvent event) {
-			return false;
-		}
-
-		public Boolean visit(UnblockEvent event) {
-			return false;
-		}
+	/**
+	 * Returns true if an event should be skipped.
+	 */
+	private static final Visitor<Boolean> SKIP = new Visitor<Boolean>() {
+		public Boolean visit(AfterEvent event) { return false; }
+		public Boolean visit(BeforeEvent event) { return true; }
+		public Boolean visit(BlockEvent event) { return false; }
+		public Boolean visit(ChokeEvent event) { return false; }
+		public Boolean visit(DieEvent event) { return false; }
+		public Boolean visit(ErrorEvent event) { return false; }
+		public Boolean visit(ForkEvent event) { return false; }
+		public Boolean visit(FreeEvent event) { return true; }
+		public Boolean visit(PrintEvent event) { return false; }
+		public Boolean visit(PublishEvent event) { return false; }
+		public Boolean visit(PullEvent event) { return true; }
+		public Boolean visit(ReceiveEvent event) { return false; }
+		public Boolean visit(SendEvent event) { return false; }
+		public Boolean visit(StoreEvent event) { return false; }
+		public Boolean visit(UnblockEvent event) { return false; }
+	};
+	
+	/**
+	 * Simulate an event either forwards or backwards.
+	 * Most events have no effect.
+	 */
+	private abstract class Simulator implements Visitor<Void> {
+		public Void visit(AfterEvent event) { return null; }
+		public Void visit(BeforeEvent event) { return null; }
+		public Void visit(BlockEvent event) { return null; }
+		public Void visit(ChokeEvent event) { return null; }
+		public Void visit(DieEvent event) { return null; }
+		public Void visit(ErrorEvent event) { return null; }
+		public Void visit(FreeEvent event) { return null; }
+		public Void visit(PrintEvent event) { return null; }
+		public Void visit(PublishEvent event) { return null; }
+		public Void visit(PullEvent event) { return null; }
+		public Void visit(ReceiveEvent event) { return null; }
+		public Void visit(SendEvent event) { return null; }
+		public Void visit(StoreEvent event) { return null; }
+		public Void visit(UnblockEvent event) { return null; }
 	}
 
 	private class ForwardSimulator extends Simulator {
-		public Boolean visit(ForkEvent event) {
+		public Void visit(ForkEvent event) {
 			if (getThreadState(event.getThread()) != null) {
 				addThread(event, event);
 			}
-			return false;
+			return null;
 		}
 	}
 	
 	private class BackwardSimulator extends Simulator {
-		public Boolean visit(ForkEvent event) {
+		public Void visit(ForkEvent event) {
 			if (getThreadState(event) != null) {
 				unforkThread(event);
 			}
-			return false;
+			return null;
 		}
 	}
 	
-	private boolean simulate(Event event, Simulator sim) {
-		final ThreadState state = getThreadState(event.getThread());
-		boolean skip = event.accept(sim);
+	/**
+	 * Return true if this event should be skipped when stepping.
+	 */
+	private boolean isBoring(Event event) {
+		return event.accept(SKIP) || event.getThread() != threads.get(0);
+	}
+	
+	/**
+	 * Simulate an event and return false when simulation
+	 * should stop.
+	 */
+	private void simulate(Event event, Simulator sim) {
+		event.accept(sim);
+		ThreadState state = getThreadState(event.getThread());
 		if (state != null) {
 			state.lastEvent = event;
 		}
-		return skip || event.getThread() != threads.get(0);
+	}
+
+	@Option(name="-help",usage="Show command-line argument usage")
+	public void printUsage(boolean _) throws CmdLineException{
+		throw new CmdLineException("");
+	}
+	
+	public void processArgs(String[] args) {
+		CmdLineParser parser = new CmdLineParser(this); 
+		try {
+			parser.parseArgument(args);
+		} catch (CmdLineException e1) {
+			System.err.println(e1.getMessage());
+			System.err.println("Usage: java -cp orc.jar " +
+					getClass().getName() +" [options] [file]");
+			parser.printUsage(System.err);
+			System.exit(1);
+		}
+	}
+	
+	public void run() throws IOException {
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		print();
+		loop: while (true) {
+			System.out.print("> ");
+			String command = r.readLine();
+			if (command == null) break loop;
+			if (command.length() == 0) command = "?";
+			switch (command.charAt(0)) {
+			case 'q':
+				break loop;
+			case 'p':
+				print();
+				break;
+			case 'f':
+				try {
+					forward();
+					print();
+				} catch (EndOfStream e) {
+					System.err.println("No more events.");
+				}
+				break;
+			case 'b':
+				try {
+					backward();
+					print();
+				} catch (EndOfStream e) {
+					System.err.println("No more events.");
+				}
+				break;
+			case 't':
+				try {
+					int index = Integer.parseInt(command.substring(2));
+					if (index >= threads.size()) {
+						System.err.println("Bad index " + index);
+					} else {
+						swapThreads(0, index);
+						forward();
+						print();
+					}
+				} catch (NumberFormatException e) {
+					System.err.println("Malformed index '" + command.substring(2));
+				} catch (EndOfStream e) {
+					System.err.println("No more events.");
+				}
+				break;
+			default:
+				System.out.println("Please enter one of the following commands:");
+				System.out.println("q: quit");
+				System.out.println("f: step forward");
+				System.out.println("b: step backward");
+				System.out.println("p: print current state");
+				System.out.println("t N: switch to thread N");
+				break;
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Debugger d = new Debugger();
+		d.processArgs(args);
+		d.run();
 	}
 }
