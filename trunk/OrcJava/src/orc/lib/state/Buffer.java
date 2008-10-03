55c28259fdbb299b55882fd35cb192150133aa1b
adrianquark <adrianquark@440195e8-269f-11de-a2b9-47ed1c40409f>
2008-10-03 23:58:59 +0000

    Fixing Buffer#close() so that subsequent puts and gets (once the buffer becomes empty) die.

--- trunk/OrcJava/src/orc/lib/state/Buffer.java
+++ trunk/OrcJava/src/orc/lib/state/Buffer.java
@@ -34,6 +34,11 @@ public class Buffer extends EvalSite {
 
 		private LinkedList<Object> localBuffer;
 		private LinkedList<Token> pendingQueue;
+		/**
+		 * Once this becomes true, no new items may be put,
+		 * and gets on an empty buffer die rather than blocking.
+		 */
+		private boolean closed = false;
 
 		BufferInstance() {
 			localBuffer = new LinkedList<Object>();
@@ -42,8 +47,37 @@ public class Buffer extends EvalSite {
 		
 		@Override
 		protected void addMethods() {
-			addMethod("get", new getMethod());	
-			addMethod("put", new putMethod());
+			addMethod("get", new Site() {
+				public void callSite(Args args, Token receiver) {
+					if (localBuffer.isEmpty()) {
+						if (closed) receiver.die();
+						else pendingQueue.addLast(receiver);
+					} else {
+						// If there is an item available, pop it and return it.
+						receiver.resume(localBuffer.removeFirst());
+					}
+				}
+			});	
+			addMethod("put", new Site() {
+				@Override
+				public void callSite(Args args, Token sender) throws TokenException {
+					Object item = args.getArg(0);
+					if (closed) {
+						sender.die();
+						return;
+					}
+					if (pendingQueue.isEmpty()) {
+						// If there are no waiting callers, buffer this item.
+						localBuffer.addLast(item);
+					} else {
+						// If there are callers waiting, give this item to the top caller.
+						Token receiver = pendingQueue.removeFirst();
+						receiver.resume(item);
+					}
+					// Since this is an asynchronous buffer, a put call always returns.
+					sender.resume();
+				}
+			});
 			addMethod("getnb", new Site() {
 				@Override
 				public void callSite(Args args, Token receiver) {
@@ -54,12 +88,21 @@ public class Buffer extends EvalSite {
 			addMethod("getAll", new EvalSite() {
 				@Override
 				public Object evaluate(Args args) throws TokenException {
-					return ListValue.make(localBuffer);
+					Object out = ListValue.make(localBuffer);
+					localBuffer.clear();
+					return out;
+				}
+			});	
+			addMethod("isClosed", new EvalSite() {
+				@Override
+				public Object evaluate(Args args) throws TokenException {
+					return closed;
 				}
 			});	
 			addMethod("close", new EvalSite() {
 				@Override
 				public Object evaluate(Args args) throws TokenException {
+					closed = true;
 					for (Token pending : pendingQueue) {
 						pending.die();
 					}
@@ -67,44 +110,5 @@ public class Buffer extends EvalSite {
 				}
 			});	
 		}
-		
-		private class getMethod extends Site {
-			@Override
-			public void callSite(Args args, Token receiver) {
-
-				// If there are no buffered items, put this caller on the queue
-				if (localBuffer.isEmpty()) {
-					pendingQueue.addLast(receiver);
-				}
-				// If there is an item available, pop it and return it.
-				else {
-					receiver.resume(localBuffer.removeFirst());
-				}
-
-			}
-		}
-		
-		private class putMethod extends Site {
-			@Override
-			public void callSite(Args args, Token sender) throws TokenException {
-
-				Object item = args.getArg(0);
-				
-				// If there are no waiting callers, buffer this item.
-				if (pendingQueue.isEmpty()) {
-					localBuffer.addLast(item);
-				}
-				// If there are callers waiting, give this item to the top caller.
-				else {
-					Token receiver = pendingQueue.removeFirst();
-					receiver.resume(item);
-				}
-
-				// Since this is an asynchronous buffer, a put call always returns.
-				sender.resume();
-			}
-		}
-
 	}
-	
 }
