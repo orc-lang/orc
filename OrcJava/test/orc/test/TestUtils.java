9f2c1988e2f2f0085e51daf0a4b9f074e683bc3f
Adrian Quark <adrian@sixfingeredman.net>
2008-10-03 03:29:38 +0000

    TestUtils find files function shouldn't barf if dir doesn't exist.

--- OrcJava/test/orc/test/TestUtils.java
+++ OrcJava/test/orc/test/TestUtils.java
@@ -6,7 +6,9 @@ import java.util.LinkedList;
 public final class TestUtils {
 	private TestUtils() {}
 	public static void findOrcFiles(File base, LinkedList<File> files) {
-		for (File file : base.listFiles()) {
+		File[] list = base.listFiles();
+		if (list == null) return;
+		for (File file : list) {
 			if (file.isDirectory()) {
 				findOrcFiles(file, files);
 			} else if (file.getPath().endsWith(".orc")) {
