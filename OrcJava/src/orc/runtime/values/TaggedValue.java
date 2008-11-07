6d8b5aa4ff18dc147a2ab5a24b0237485bb2dba6
Adrian Quark <adrian@sixfingeredman.net>
2008-11-07 02:57:11 +0000

    Tracer traces tagged values.

--- OrcJava/src/orc/runtime/values/TaggedValue.java
+++ OrcJava/src/orc/runtime/values/TaggedValue.java
@@ -1,18 +1,20 @@
 package orc.runtime.values;
 
+import java.util.Arrays;
+
 import orc.runtime.sites.Site;
 import orc.runtime.sites.core.Datasite;
 
 public class TaggedValue extends Value {
-	public TupleValue payload;
+	public Object[] values;
 	public Datasite tag; // typically this is a reference to the injection site itself
 	
-	public TaggedValue(TupleValue payload, Datasite tag) {
-		this.payload = payload;
+	public TaggedValue(Datasite tag, Object[] values) {
+		this.values = values;
 		this.tag = tag;
 	}
 	
 	public String toString() {
-		return tag.tagname + payload;
+		return tag.tagName + Arrays.toString(values);
 	}
 }
