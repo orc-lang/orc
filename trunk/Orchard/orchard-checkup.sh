e5af089548ce72fe3b5ab04127db09e05ede76ec
adrianquark <adrianquark@440195e8-269f-11de-a2b9-47ed1c40409f>
2008-12-25 04:32:04 +0000

    Adding checkup script

---
+++ trunk/Orchard/orchard-checkup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This program runs a simple test of an Orchard server to
+# make sure it is running and compiling programs correctly.
+#
+# Place it somewhere like /usr/local/bin
+# Then add the following to /etc/cron.d/orchard:
+# 0,15,30,45 * * * * root /usr/local/bin/orchard-checkup
+
+URL="http://orc.csres.utexas.edu/orchard/json/executor"
+EMAIL="oncall@orc.csres.utexas.edu"
+
+# report an error and halt
+error() {
+        echo "$1" | mail -s "orc.csres.utexas.edu problem" "$EMAIL"
+        exit 1
+}
+
+# post a request
+post() {
+        curl -s -d "$1" "$URL"
+}
+
+# post a request, compare against a response glob,
+# and report an error if it fails
+check() {
+        [[ "$1" == $2 ]] || error "$3: $1"
+}
+
+JOBQ="$(post "{compileAndSubmit:{program:'1'}}")"
+check "$JOBQ" "\"*-*\"" "Could not create job"
+
+JOB="$(eval echo $JOBQ)"
+check "$(post "{startJob:{job:'$JOB'}}")" "null" "Could not start job"
+check "$(post "{jobEvents:{job:'$JOB'}}")" "*value\":{\"@xsi.type\":\"xs:integer\",\"$\":\"1\"}*" "Unexpected events"
+check "$(post "{finishJob:{job:'$JOB'}}")" "null" "Could not halt job"
