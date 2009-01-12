e17352bdb096cc5f8a781c805d1a24cd547b733f
Adrian Quark <adrian@sixfingeredman.net>
2009-01-12 22:00:42 +0000

    Add runnable=false option for code snippets.

--- OrcJSPWikiPlugin/src/orc/jspwiki/Orc.java
+++ OrcJSPWikiPlugin/src/orc/jspwiki/Orc.java
@@ -7,6 +7,28 @@ import com.ecyrd.jspwiki.WikiContext;
 import com.ecyrd.jspwiki.plugin.PluginException;
 import com.ecyrd.jspwiki.plugin.WikiPlugin;
 
+/**
+ * This is a plugin for JSPWiki which enables syntax like this:
+ * <pre>
+ * [{orc
+ * 
+ * -- your program here
+ * }]
+ * </pre>
+ * 
+ * <p>This plugin supports the properties:
+ * <ul>
+ * <li>runnable: if "false", program is not runnable.
+ * <li>editable: if "true", program is editable.
+ * <li>height: a CSS height (e.g. "100px"). If this is not given, the box will be just big enough to contain the existing program text.
+ * <li>baseURL: URL path to Orchard installation (defaults to "/orchard/")
+ * </ul>
+ * 
+ * <p>To install, "ant jar", place the resulting jar in the WEB-INF/lib directory of jspwiki, and restart your servlet container.
+ * 
+ * @author quark
+ *
+ */
 public class Orc implements WikiPlugin {
 	private final static String HAS_ORC = "orc.jspwiki.HAS_ORC";
 	public String execute(WikiContext ctx, Map props) throws PluginException {
@@ -28,10 +50,15 @@ public class Orc implements WikiPlugin {
 			tag = "textarea";
 		}
 		String height = (String)props.get("height");
+		String runnable = (String)props.get("runnable");
 		String body = (String)props.get("_body");
 		if (body != null) {
 			body = TextUtil.replaceEntities(body);
-			out.append("<"+tag+" class=\"orc\"");
+			out.append("<"+tag+" class=\"orc");
+			if (runnable != null && runnable.equals("false")) {
+				out.append("-snippet");
+			}
+			out.append("\"");
 			if (height != null) {
 				out.append(" style=\"height: " + height + "\"");
 			}
