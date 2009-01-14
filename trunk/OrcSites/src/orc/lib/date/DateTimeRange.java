789a0d856191b0205252435b03a00a03d2496e29
adrianquark <adrianquark@440195e8-269f-11de-a2b9-47ed1c40409f>
2009-01-14 22:16:13 +0000

    Rename DateRange and friends to DateTimeRange. Get rid of unnecessary joda-time wrapper sites.

--- trunk/OrcSites/src/orc/lib/date/DateTimeRange.java
+++ trunk/OrcSites/src/orc/lib/date/DateTimeRange.java
@@ -1,36 +1,49 @@
 package orc.lib.date;
 
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
 
 /** The end date is considered exclusive. */
-public class DateRange implements Comparable<DateRange> {
-	public final static DateRange NULL;
+public final class DateTimeRange implements Comparable<DateTimeRange> {
+	public final static DateTimeRange NULL;
 	static {
-		LocalDateTime dummy = new LocalDateTime();
-		NULL = new DateRange(dummy, dummy);
+		DateTime dummy = new DateTime();
+		NULL = new DateTimeRange(dummy, dummy);
 	}
-	public LocalDateTime start;
-	public LocalDateTime end;
-	public DateRange(LocalDateTime start, LocalDateTime end) {
+	public DateTime start;
+	public DateTime end;
+	public DateTimeRange(DateTime start, DateTime end) {
 		super();
 		this.start = start;
 		this.end = end;
 	}
-	public void union(DateRange that) {
+	public DateTimeRange(LocalDateTime start, LocalDateTime end) {
+		this(start.toDateTime(), end.toDateTime());
+	}
+	public DateTimeRange(LocalDate start, LocalDate end) {
+		this(start.toDateMidnight().toDateTime(), end.toDateMidnight().toDateTime());
+	}
+	public void union(DateTimeRange that) {
 		if (that.start.compareTo(this.start) < 0) this.start = that.start;
 		if (that.end.compareTo(this.end) > 0) this.end = that.end;
 	}
-	public void intersect(DateRange that) {
+	public void intersect(DateTimeRange that) {
 		if (that.start.compareTo(this.start) > 0) this.start = that.start;
 		if (that.end.compareTo(this.end) < 0) this.end = that.end;
+	}
+	public boolean intersects(DateTimeRange that) {
+		if (that.start.compareTo(this.end) >= 0) return false;
+		if (that.end.compareTo(this.start) <= 0) return false;
+		return true;
 	}
 	public boolean isEmpty() {
 		return start.compareTo(end) >= 0;
 	}
-	public LocalDateTime getStart() {
+	public DateTime getStart() {
 		return start;
 	}
-	public LocalDateTime getEnd() {
+	public DateTime getEnd() {
 		return end;
 	}
 	/**
@@ -40,7 +53,7 @@ public class DateRange implements Comparable<DateRange> {
 	 * 
 	 * <p>Also note that this ordering is "inconsistent with equals".
 	 */
-	public int compareTo(DateRange that) {
+	public int compareTo(DateTimeRange that) {
 		if (that.end.compareTo(this.start) < 0) return 1;
 		if (that.start.compareTo(this.end) > 0) return -1;
 		return 0;
