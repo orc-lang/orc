b63f75b33f0c8761cb2a5f47fbef5205cbdbcd3d
Adrian Quark <adrian@sixfingeredman.net>
2009-01-14 22:16:13 +0000

    Rename DateRange and friends to DateTimeRange. Get rid of unnecessary joda-time wrapper sites.

--- OrcSites/src/orc/lib/date/DateTimeRanges.java
+++ OrcSites/src/orc/lib/date/DateTimeRanges.java
@@ -4,24 +4,24 @@ import java.util.Iterator;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-import org.joda.time.LocalDateTime;
+import org.joda.time.DateTime;
 
-public class DateRanges {
-	private SortedSet<DateRange> ranges = new TreeSet<DateRange>();
-	public DateRanges() {}
+public class DateTimeRanges {
+	private SortedSet<DateTimeRange> ranges = new TreeSet<DateTimeRange>();
+	public DateTimeRanges() {}
 	
-	public DateRanges(DateRange range) {
+	public DateTimeRanges(DateTimeRange range) {
 		ranges.add(range);
 	}
 
-	public void union(DateRanges that) {
-		SortedSet<DateRange> tailSet = ranges;
-		for (DateRange range : that.ranges) {
+	public void union(DateTimeRanges that) {
+		SortedSet<DateTimeRange> tailSet = ranges;
+		for (DateTimeRange range : that.ranges) {
 			// find all of the ranges overlapping this one
 			// and replace them with a new unified range
 			tailSet = tailSet.tailSet(range);
-			for (Iterator<DateRange> i = tailSet.iterator(); i.hasNext();) {
-				DateRange r = i.next();
+			for (Iterator<DateTimeRange> i = tailSet.iterator(); i.hasNext();) {
+				DateTimeRange r = i.next();
 				if (r.compareTo(range) > 0) break;
 				i.remove();
 				range.union(r);
@@ -30,15 +30,15 @@ public class DateRanges {
 		}
 	}
 	
-	public void intersect(DateRanges that) {
-		SortedSet<DateRange> tailSet = ranges;
-		ranges = new TreeSet<DateRange>();
-		for (DateRange range : that.ranges) {
+	public void intersect(DateTimeRanges that) {
+		SortedSet<DateTimeRange> tailSet = ranges;
+		ranges = new TreeSet<DateTimeRange>();
+		for (DateTimeRange range : that.ranges) {
 			// find all of the ranges overlapping this one, intersect
 			// each with this one, and add them to the result
 			tailSet = tailSet.tailSet(range);
-			for (Iterator<DateRange> i = tailSet.iterator(); i.hasNext();) {
-				DateRange r = i.next();
+			for (Iterator<DateTimeRange> i = tailSet.iterator(); i.hasNext();) {
+				DateTimeRange r = i.next();
 				if (r.compareTo(range) > 0) break;
 				r.intersect(range);
 				if (!r.isEmpty()) ranges.add(r);
@@ -46,21 +46,21 @@ public class DateRanges {
 		}
 	}
 	
-	public boolean spans(LocalDateTime date) {
-		for (DateRange r : ranges.tailSet(new DateRange(date, date))) {
+	public boolean spans(DateTime date) {
+		for (DateTimeRange r : ranges.tailSet(new DateTimeRange(date, date))) {
 			return (r.start.compareTo(date) <= 0);
 		}
 		return false;
 	}
 	
-	public SortedSet<DateRange> getRanges() {
+	public SortedSet<DateTimeRange> getRanges() {
 		return ranges;
 	}
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("DateRanges{\n");
-		for (DateRange r : ranges) {
+		for (DateTimeRange r : ranges) {
 			sb.append(r.toString() + "\n");
 		}
 		sb.append("}");
