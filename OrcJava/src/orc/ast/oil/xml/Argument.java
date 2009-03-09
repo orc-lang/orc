0bae4ce52ac9487f1a76af1a075c280503c77139
Adrian Quark <adrian@sixfingeredman.net>
2009-03-09 23:09:15 +0000

    Fixing OIL->XML serialization error with Orc field values.

--- OrcJava/src/orc/ast/oil/xml/Argument.java
+++ OrcJava/src/orc/ast/oil/xml/Argument.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlSeeAlso;
  * Arguments to sites and expressions.
  * @author quark
  */
-@XmlSeeAlso(value={Constant.class, Site.class, Variable.class})
+@XmlSeeAlso(value={Constant.class, Field.class, Site.class, Variable.class})
 public abstract class Argument extends Expression {
 	@Override
 	public abstract orc.ast.oil.arg.Arg unmarshal();
