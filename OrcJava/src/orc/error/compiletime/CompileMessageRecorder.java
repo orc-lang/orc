580ba06ad45d6a42241fe6f5e67874066444d73f
John A. Thywissen <jthywiss@users.noreply.github.com>
2009-08-20 14:51:07 +0000

    Add convenience methods to ComplieMessageRecorder interface. JavaDoc clean-up.

--- OrcJava/src/orc/error/compiletime/CompileMessageRecorder.java
+++ OrcJava/src/orc/error/compiletime/CompileMessageRecorder.java
@@ -13,6 +13,7 @@ import orc.error.SourceLocation;
 
 /**
  * Interface to environment's message mechanism for compiler diagnostics.
+ * This my be writing to stderr, updating a GUI compile status window, or the like.
  * 
  * @author jthywiss
  */
@@ -59,14 +60,68 @@ public interface CompileMessageRecorder {
 		INTERNAL
 	}
 	
+	/**
+	 * Record that compile processing has begun for the given file.
+	 * This also resets maxSeverity.
+	 * Do not call for included files, only for the "main" file that the compiler was invoked on.
+	 *  
+	 * @param file File for which compiler processing is beginning
+	 */
 	public void beginProcessing(File file);
 	
+	/**
+	 * Record a compile problem message.  This message is forwarded to the environment in
+	 * a manner specific to the implementing class.
+	 * 
+	 * @param severity {@link Severity} of this message
+	 * @param code integer code of this message (potentially used for filtering, is supported by the environment}
+	 * @param message String localized message specifically describing problem for the complier user
+	 * @param location {@link SourceLocation} where problem occurred. Indicate unknown location with a partially filled SourceLocation.
+	 * @param astNode {@link ASTNode} subtree containing problem, or null if not applicable or not available.
+	 * @param exception {@link Throwable} indicating problem, or null if not applicable or not available.
+	 */
 	public void recordMessage(Severity severity, int code, String message, SourceLocation location, Object astNode, Throwable exception);
-	//TODO: Decide on some subset of parms above
-	//TODO: Declare convenience methods
-	//TODO: JavaDoc
+
+	/**
+	 * Convenience method, equivalent to <code>recordMessage(severity, code, message, location, null, exception)</code>
+	 * 
+	 * @param severity {@link Severity} of this message
+	 * @param code integer code of this message (potentially used for filtering, is supported by the environment}
+	 * @param message String localized message specifically describing problem for the complier user
+	 * @param location {@link SourceLocation} where problem occurred. Indicate unknown location with a partially filled SourceLocation.
+	 * @param exception {@link Throwable} indicating problem, or null if not applicable or not available.
+	 */
+	public void recordMessage(Severity severity, int code, String message, SourceLocation location, Throwable exception);
+
+	/**
+	 * Convenience method, equivalent to <code>recordMessage(severity, code, message, location, astNode, null)</code>
+	 * 
+	 * @param severity {@link Severity} of this message
+	 * @param code integer code of this message (potentially used for filtering, is supported by the environment}
+	 * @param message String localized message specifically describing problem for the complier user
+	 * @param location {@link SourceLocation} where problem occurred. Indicate unknown location with a partially filled SourceLocation.
+	 * @param astNode {@link ASTNode} subtree containing problem, or null if not applicable or not available.
+	 */
+	public void recordMessage(Severity severity, int code, String message, SourceLocation location, Object astNode);
 	
+	/**
+	 * Convenience method, equivalent to <code>recordMessage(severity, code, message, null, null, null)</code>
+	 * 
+	 * @param severity {@link Severity} of this message
+	 * @param code integer code of this message (potentially used for filtering, is supported by the environment}
+	 * @param message String localized message specifically describing problem for the complier user
+	 */
+	public void recordMessage(Severity severity, int code, String message);
+	
+	/**
+	 * @return maximum severity of messages recoded since {@link #beginProcessing(File)} was invoked
+	 */
 	public Severity getMaxSeverity();
 	
+	/**
+	 * Record that compile processing is complete for the given file.
+	 * 
+	 * @param file File for which compiler processing is complete
+	 */
 	public void endProcessing(File file);
 }
