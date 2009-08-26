724945eb94c129b023b01fe530a1ee4480c4c3e1
John A. Thywissen <jthywiss@users.noreply.github.com>
2009-08-26 21:06:33 +0000

    Add AnnotationHover service to fix the following IMP bug: editor displaying quickdiff "line added", etc. annotations.

---
+++ OrcEclipse/src/edu/utexas/cs/orc/orceclipse/edit/OrcAnnotationHover.java
@@ -0,0 +1,50 @@
+//
+// OrcAnnotationHover.java -- Java class OrcAnnotationHover
+// Project OrcEclipse
+//
+// $Id$
+//
+// Created by jthywiss on Aug 26, 2009.
+//
+// Copyright (c) 2009 The University of Texas at Austin. All rights reserved.
+//
+// Use and redistribution of this file is governed by the license terms in
+// the LICENSE file found in the project's top-level directory and also found at
+// URL: http://orc.csres.utexas.edu/license.shtml .
+//
+
+package edu.utexas.cs.orc.orceclipse.edit;
+
+import java.util.List;
+
+import org.eclipse.imp.editor.AnnotationHoverBase;
+import org.eclipse.jface.text.source.Annotation;
+import org.eclipse.imp.services.IAnnotationHover;
+import org.eclipse.jface.text.source.ISourceViewer;
+
+/**
+ * Provides the text for a hover over an annotation in the editor.
+ * <p>
+ * This subclass exists solely to fix IMP bugs.
+ *
+ * @author jthywiss
+ */
+public class OrcAnnotationHover extends AnnotationHoverBase implements IAnnotationHover {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.imp.editor.AnnotationHoverBase#getHoverInfo(org.eclipse.jface.text.source.ISourceViewer, int)
+	 */
+	@Override
+	public String getHoverInfo(ISourceViewer sourceViewer, int lineNumber) {
+        List<Annotation> srcAnnotations= getSourceAnnotationsForLine(sourceViewer, lineNumber);
+        
+        for (Annotation annotation : srcAnnotations) {
+        	if (annotation.getType().startsWith("org.eclipse.ui.workbench.texteditor.quickdiff")) {
+        		srcAnnotations.remove(annotation);
+        	}
+        }
+
+        return formatAnnotationList(srcAnnotations);
+	}
+
+}
