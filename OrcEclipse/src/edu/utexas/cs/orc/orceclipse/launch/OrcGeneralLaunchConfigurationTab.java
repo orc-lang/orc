9ecc2bf7fea23bf81bab8ac79e31bcf3f812da74
John A. Thywissen <jthywiss@users.noreply.github.com>
2009-09-07 05:09:28 +0000

    OrcBuilder and OrcParseContoller now use OrcConfigSettings Remove type check and no prelude settings from Orc launch configuration Add max. pubs., num. site threads, trace output file name, and debug level settings to Orc launch configuration Remove JDT Java args., JRE, and class path tabs from Orc launch configuration tab group Add new OrcProjectPropertiesPage, with type check, no prelude, include path, and site class path settings Fix missing icon bug on General tab of Orc launch configuration Version of Eclipse required is declared as 3.3 Version bump

--- OrcEclipse/src/edu/utexas/cs/orc/orceclipse/launch/OrcGeneralLaunchConfigurationTab.java
+++ OrcEclipse/src/edu/utexas/cs/orc/orceclipse/launch/OrcGeneralLaunchConfigurationTab.java
@@ -16,19 +16,21 @@
 package edu.utexas.cs.orc.orceclipse.launch;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.internal.ui.SWTFactory;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.statushandlers.StatusManager;
+import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.swt.widgets.Text;
 
 import edu.utexas.cs.orc.orceclipse.Activator;
 import edu.utexas.cs.orc.orceclipse.OrcConfigSettings;
@@ -41,12 +43,15 @@ import edu.utexas.cs.orc.orceclipse.OrcResources;
  * @see org.eclipse.debug.ui.ILaunchConfigurationTab
  * @see org.eclipse.debug.ui.AbstractLaunchConfigurationTab
  */
+@SuppressWarnings("restriction")
 public class OrcGeneralLaunchConfigurationTab extends AbstractLaunchConfigurationTab {
 
 	private static Image ORC_PLUGIN_ICON_IMAGE = Activator.getInstance().getImageRegistry().get(OrcResources.ORC_PLUGIN_ICON);
 
-	private Button typeCheckCheckButton;
-	private Button noPreludeCheckButton;
+	private Spinner maxPubsSpinner;
+	private Spinner numSiteThreadsSpinner;
+	private Text traceOutFilenameText;
+	private Spinner debugLevelSpinner;
 
 	/**
 	 * Constructs an OrcGeneralLaunchConfigurationTab instance.
@@ -84,6 +89,15 @@ public class OrcGeneralLaunchConfigurationTab extends AbstractLaunchConfiguratio
 	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#setDefaults(org.eclipse.debug.core.ILaunchConfigurationWorkingCopy)
 	 */
 	public void setDefaults(final ILaunchConfigurationWorkingCopy configuration) {
+		configuration.removeAttribute(OrcConfigSettings.TYPE_CHECK_ATTR_NAME);
+		configuration.removeAttribute(OrcConfigSettings.NO_PRELUDE_ATTR_NAME);
+		configuration.removeAttribute(OrcConfigSettings.INCLUDE_PATH_ATTR_NAME);
+		configuration.removeAttribute(OrcConfigSettings.SITE_CLASSPATH_ATTR_NAME);
+		configuration.removeAttribute(OrcConfigSettings.OIL_OUT_ATTR_NAME);
+		configuration.removeAttribute(OrcConfigSettings.MAX_PUBS_ATTR_NAME);
+		configuration.removeAttribute(OrcConfigSettings.NUM_SITE_THREADS_ATTR_NAME);
+		configuration.removeAttribute(OrcConfigSettings.TRACE_OUT_ATTR_NAME);
+		configuration.removeAttribute(OrcConfigSettings.DEBUG_LEVEL_ATTR_NAME);
 		OrcLaunchDelegate.setDefaults(configuration);
 	}
 
@@ -94,28 +108,52 @@ public class OrcGeneralLaunchConfigurationTab extends AbstractLaunchConfiguratio
 		final Composite comp = new Composite(parent, SWT.NONE);
 		setControl(comp);
 		comp.setLayout(new GridLayout());
-		SelectionAdapter ourSelectionAdapter = new SelectionAdapter() {
+		final SelectionAdapter ourSelectionAdapter = new SelectionAdapter() {
 			@Override
 			public void widgetSelected(final SelectionEvent e) {
 				widgetSelectedAction(e);
 			}
 		};
+		final ModifyListener ourModifyListener = new ModifyListener() {
+			public void modifyText(final ModifyEvent e) {
+				widgetModifiedAction(e);
+			}
+		};
 
-		typeCheckCheckButton = createCheckButton(comp, "Type check");
-		typeCheckCheckButton.addSelectionListener(ourSelectionAdapter);
+		final Composite labelWidgetComp = SWTFactory.createComposite(comp, comp.getFont(), 2, 1, GridData.FILL_HORIZONTAL, 0, 0);
 
-		noPreludeCheckButton = createCheckButton(comp, "Do not include standard prelude");
-		noPreludeCheckButton.addSelectionListener(ourSelectionAdapter);
+		SWTFactory.createLabel(labelWidgetComp, "Maximum allowed number of publications:", 1);
 
-		// TODO: Finish adding controls for the remaining attributes
-		// - OIL output file
-		// - Maximum allowed number of publications
-		// - Number of site call threads
-		// - Trace output file name
-		// - Debug level 0 to 4
+		maxPubsSpinner = new Spinner(labelWidgetComp, SWT.NONE);
+		maxPubsSpinner.setFont(parent.getFont());
+		maxPubsSpinner.setValues(OrcConfigSettings.MAX_PUBS_DEFAULT, 0, Integer.MAX_VALUE, 0, 1, 100);
+		maxPubsSpinner.addSelectionListener(ourSelectionAdapter);
+
+		SWTFactory.createLabel(labelWidgetComp, "Number of site call threads:", 1);
+
+		numSiteThreadsSpinner = new Spinner(labelWidgetComp, SWT.NONE);
+		numSiteThreadsSpinner.setFont(parent.getFont());
+		numSiteThreadsSpinner.setValues(OrcConfigSettings.NUM_SITE_THREADS_DEFAULT, 1, Integer.MAX_VALUE, 0, 1, 10);
+		numSiteThreadsSpinner.addSelectionListener(ourSelectionAdapter);
+
+		SWTFactory.createLabel(labelWidgetComp, "Trace output file name:", 1);
+
+		traceOutFilenameText = SWTFactory.createSingleText(labelWidgetComp, 1);
+		traceOutFilenameText.addModifyListener(ourModifyListener);
+
+		SWTFactory.createLabel(labelWidgetComp, "Orc engine debug level:", 1);
+
+		debugLevelSpinner = new Spinner(labelWidgetComp, SWT.NONE);
+		debugLevelSpinner.setFont(parent.getFont());
+		debugLevelSpinner.setValues(OrcConfigSettings.DEBUG_LEVEL_DEFAULT, 0, 4, 0, 1, 1);
+		debugLevelSpinner.addSelectionListener(ourSelectionAdapter);
 	}
 
 	protected void widgetSelectedAction(final SelectionEvent e) {
+		updateLaunchConfigurationDialog();
+	}
+
+	protected void widgetModifiedAction(final ModifyEvent e) {
 		updateLaunchConfigurationDialog();
 	}
 
@@ -124,14 +162,10 @@ public class OrcGeneralLaunchConfigurationTab extends AbstractLaunchConfiguratio
 	 */
 	public void initializeFrom(final ILaunchConfiguration configuration) {
 		try {
-			typeCheckCheckButton.setSelection(configuration.getAttribute(OrcConfigSettings.TYPE_CHECK_ATTR_NAME, OrcConfigSettings.TYPE_CHECK_DEFAULT));
-			noPreludeCheckButton.setSelection(configuration.getAttribute(OrcConfigSettings.NO_PRELUDE_ATTR_NAME, OrcConfigSettings.NO_PRELUDE_DEFAULT));
-			// TODO: Finish initializing controls for the remaining attributes
-			//.setSelection(configuration.getAttribute(OrcConfigSettings.OIL_OUT_ATTR_NAME, OrcConfigSettings.OIL_OUT_DEFAULT));
-			//.setSelection(configuration.getAttribute(OrcConfigSettings.MAX_PUBS_ATTR_NAME, OrcConfigSettings.MAX_PUBS_DEFAULT));
-			//.setSelection(configuration.getAttribute(OrcConfigSettings.NUM_SITE_THREADS_ATTR_NAME, OrcConfigSettings.NUM_SITE_THREADS_DEFAULT));
-			//.setSelection(configuration.getAttribute(OrcConfigSettings.TRACE_OUT_ATTR_NAME, OrcConfigSettings.TRACE_OUT_DEFAULT));
-			//.setSelection(configuration.getAttribute(OrcConfigSettings.DEBUG_LEVEL_ATTR_NAME, OrcConfigSettings.DEBUG_LEVEL_DEFAULT));
+			maxPubsSpinner.setSelection(configuration.getAttribute(OrcConfigSettings.MAX_PUBS_ATTR_NAME, OrcConfigSettings.MAX_PUBS_DEFAULT));
+			numSiteThreadsSpinner.setSelection(configuration.getAttribute(OrcConfigSettings.NUM_SITE_THREADS_ATTR_NAME, OrcConfigSettings.NUM_SITE_THREADS_DEFAULT));
+			traceOutFilenameText.setText(configuration.getAttribute(OrcConfigSettings.TRACE_OUT_ATTR_NAME, OrcConfigSettings.TRACE_OUT_DEFAULT));
+			debugLevelSpinner.setSelection(configuration.getAttribute(OrcConfigSettings.DEBUG_LEVEL_ATTR_NAME, OrcConfigSettings.DEBUG_LEVEL_DEFAULT));
 		} catch (final CoreException e) {
 			Activator.logAndShow(e);
 		}
@@ -150,14 +184,26 @@ public class OrcGeneralLaunchConfigurationTab extends AbstractLaunchConfiguratio
 	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#performApply(org.eclipse.debug.core.ILaunchConfigurationWorkingCopy)
 	 */
 	public void performApply(final ILaunchConfigurationWorkingCopy configuration) {
-		configuration.setAttribute(OrcConfigSettings.TYPE_CHECK_ATTR_NAME, typeCheckCheckButton.getSelection());
-		configuration.setAttribute(OrcConfigSettings.NO_PRELUDE_ATTR_NAME, noPreludeCheckButton.getSelection());
-		// TODO: Finish setting the remaining attributes
-		//configuration.setAttribute(OrcConfigSettings.OIL_OUT_ATTR_NAME, .getSelection());
-		//configuration.setAttribute(OrcConfigSettings.MAX_PUBS_ATTR_NAME, .getSelection());
-		//configuration.setAttribute(OrcConfigSettings.NUM_SITE_THREADS_ATTR_NAME, .getSelection());
-		//configuration.setAttribute(OrcConfigSettings.TRACE_OUT_ATTR_NAME, .getSelection());
-		//configuration.setAttribute(OrcConfigSettings.DEBUG_LEVEL_ATTR_NAME, .getSelection());
+		setOrUnsetIntAttr(configuration, OrcConfigSettings.MAX_PUBS_ATTR_NAME, maxPubsSpinner.getSelection(), OrcConfigSettings.MAX_PUBS_DEFAULT);
+		setOrUnsetIntAttr(configuration, OrcConfigSettings.NUM_SITE_THREADS_ATTR_NAME, numSiteThreadsSpinner.getSelection(), OrcConfigSettings.NUM_SITE_THREADS_DEFAULT);
+		setOrUnsetTextAttr(configuration, OrcConfigSettings.TRACE_OUT_ATTR_NAME, traceOutFilenameText.getText());
+		setOrUnsetIntAttr(configuration, OrcConfigSettings.DEBUG_LEVEL_ATTR_NAME, debugLevelSpinner.getSelection(), OrcConfigSettings.DEBUG_LEVEL_DEFAULT);
+	}
+
+	private void setOrUnsetIntAttr(final ILaunchConfigurationWorkingCopy configuration, final String attrName, final int enteredNum, final int defaultValue) {
+		if (enteredNum != defaultValue) {
+			configuration.setAttribute(attrName, enteredNum);
+		} else {
+			configuration.removeAttribute(attrName);
+		}
+	}
+
+	private void setOrUnsetTextAttr(final ILaunchConfigurationWorkingCopy configuration, final String attrName, final String enteredText) {
+		if (enteredText != null && enteredText.length() > 0) {
+			configuration.setAttribute(attrName, enteredText);
+		} else {
+			configuration.removeAttribute(attrName);
+		}
 	}
 
 	/* (non-Javadoc)
