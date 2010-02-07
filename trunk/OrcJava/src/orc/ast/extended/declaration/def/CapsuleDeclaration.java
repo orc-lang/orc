f90c20e311b445559dc82231427b0d3004598e41
amshali <amshali@440195e8-269f-11de-a2b9-47ed1c40409f>
2010-02-07 20:48:37 +0000

    Updated javadoc on CapsuleDeclaration.java

--- trunk/OrcJava/src/orc/ast/extended/declaration/def/CapsuleDeclaration.java
+++ trunk/OrcJava/src/orc/ast/extended/declaration/def/CapsuleDeclaration.java
@@ -35,14 +35,29 @@ import orc.ast.simple.expression.Expression;
 import orc.error.compiletime.CompilationException;
 
 /**
- * 
+ * This class is responsible for transforming a capsule declaration
+ * to the original Orc syntax. The details of this transformation can
+ * be found at: 
+ * <a href="http://orc.csres.utexas.edu/wiki/Wiki.jsp?page=OrcCapsules">OrcCapsules</a>
  * Feb 4, 2010
  * @author amshali
  */
 public class CapsuleDeclaration extends Declaration {
+	/**
+	 * Capsule name
+	 */
 	public String name;
+	/**
+	 * list of formal parameters
+	 */
 	public List<List<Pattern>> formals;
+	/**
+	 * body of the capsule
+	 */
 	public orc.ast.extended.expression.Expression body;
+	/**
+	 * type of formal parameters
+	 */
 	public List<String> typeFormals = null;
 
 	public CapsuleDeclaration(String name, List<List<Pattern>> formals, orc.ast.extended.expression.Expression body, List<String> typeFormals) {
@@ -54,14 +69,21 @@ public class CapsuleDeclaration extends Declaration {
 
 	@Override
 	public Expression bindto(Expression target) throws CompilationException {
-		
+
 		body = makeNewBody(body, new ArrayList<String>());
-	
+
 		DefMemberClause defMemberClause = new DefMemberClause(name, formals, body, null, typeFormals);
 
+		/* create a list of definitions with only one definition in it.
+		* which is the capsule definition.
+		*/
 		List<DefMember> defs = new ArrayList<DefMember>();
 		defs.add(defMemberClause);
 		DefsDeclaration dd = new DefsDeclaration(defs);
+		/*
+		 * use a ValDeclaration to shadow the original definition with 
+		 * a protected Site closure. 
+		 */
 		ValDeclaration vald = new ValDeclaration(new VariablePattern(name), new Call(new Name("Site"), new Name(name)));
 		return dd.bindto(vald.bindto(target));
 	}
@@ -71,9 +93,18 @@ public class CapsuleDeclaration extends Declaration {
 		return visitor.visit(this);
 	}
 
+	/**
+	 * This function recursively looks for definitions inside the 
+	 * capsule declaration (body) and put them all in defFunctions list.
+	 * At the end  
+	 * @param body Body of the capsule declaration.
+	 * @param defFunctions List of definitions inside the capsule.
+	 * @return Returns a new body
+	 * @throws CompilationException
+	 */
 	private orc.ast.extended.expression.Expression makeNewBody(orc.ast.extended.expression.Expression body, List<String> defFunctions) throws CompilationException {
 		if (body instanceof Declare) {
-			Declare decl = (Declare)body;
+			Declare decl = (Declare) body;
 			Declaration defs = decl.d;
 			if (defs instanceof DefsDeclaration) {
 				for (DefMember d : ((DefsDeclaration) defs).defs) {
@@ -91,16 +122,23 @@ public class CapsuleDeclaration extends Declaration {
 			} else {
 				List<orc.ast.extended.expression.Expression> recordArgs = makeRecordArgs(defFunctions);
 				Call recordCall = new Call(new Name("Record"), recordArgs);
-								
+
 				// Semantics: body as active process
 				return new Parallel(new Sequential(body, new Stop()), recordCall);
-				
+
 				// Alternative semantics: body as initializer
 				// return new Otherwise(new Sequential(body, new Stop()), recordCall);
 			}
 		}
 	}
 
+	/**
+	 * This method gets a list of definitions, defFunctions, and returns 
+	 * a Record site with the definitions protected by Site.
+	 * @param defFunctions
+	 * @return returns the a Record site with the name of definitions and the 
+	 * definitions protected by a Site.
+	 */
 	private List<orc.ast.extended.expression.Expression> makeRecordArgs(List<String> defFunctions) {
 		List<orc.ast.extended.expression.Expression> args = new ArrayList<orc.ast.extended.expression.Expression>();
 		for (String s : defFunctions) {
