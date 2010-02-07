3ecab65e1f19489a18154a7854bada41cd33bc18
John A. Thywissen <jthywiss@users.noreply.github.com>
2010-02-07 20:37:59 +0000

    Changes to Orchard to use Java 6 and Jetty servlet services (no longer bundling duplicate WebServices impls.) Orchard demo OS X running (finally); however, non-demo orchard and command line versions remain to be fixed. Also fixed some minor HTML syntax.

--- Orchard/src/orc/Orchard.java
+++ Orchard/src/orc/Orchard.java
@@ -13,10 +13,15 @@
 
 package orc;
 
+import javax.xml.ws.Endpoint;
+
 import orc.orchard.OrchardProperties;
 
 import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.ContextHandlerCollection;
+import org.mortbay.jetty.handler.HandlerCollection;
+import org.mortbay.jetty.j2se6.JettyHttpServerProvider;
 import org.mortbay.jetty.nio.SelectChannelConnector;
 import org.mortbay.jetty.webapp.WebAppContext;
 
@@ -29,17 +34,33 @@ public class Orchard {
 
 	public static void main(final String args[]) throws Exception {
 		OrchardProperties.setProperty("orc.lib.orchard.forms.url", "http://localhost:" + PORT + "/orchard/FormsServlet");
+
 		final Server server = new Server();
+		JettyHttpServerProvider.setServer(server);
+
 		final Connector connector = new SelectChannelConnector();
 		connector.setPort(PORT);
 		connector.setHost("127.0.0.1");
 		server.addConnector(connector);
 
-		final WebAppContext context = new WebAppContext();
-		context.setContextPath("/orchard");
-		context.setWar("./web");
-		server.setHandler(context);
+		final HandlerCollection handlers = new HandlerCollection();
+		server.setHandler(handlers);
+
+		final ContextHandlerCollection contexts = new ContextHandlerCollection();
+		handlers.addHandler(contexts);
+
+		new WebAppContext(contexts, "./web", "/orchard");
+
 		server.setStopAtShutdown(true);
 		server.start();
+
+		final Endpoint compilerSoapServiceEndpoint = Endpoint.create(new orc.orchard.soap.CompilerService());
+		compilerSoapServiceEndpoint.publish("http://localhost:" + PORT + "/orchard/soap/compiler");
+		final Endpoint executorSoapServiceEndpoint = Endpoint.create(new orc.orchard.soap.ExecutorService());
+		executorSoapServiceEndpoint.publish("http://localhost:" + PORT + "/orchard/soap/executor");
+		final Endpoint compilerJsonServiceEndpoint = Endpoint.create("https://jax-ws-commons.dev.java.net/json/", new orc.orchard.soap.CompilerService());
+		compilerJsonServiceEndpoint.publish("http://localhost:" + PORT + "/orchard/json/compiler");
+		final Endpoint executorJsonServiceEndpoint = Endpoint.create("https://jax-ws-commons.dev.java.net/json/", new orc.orchard.soap.ExecutorService());
+		executorJsonServiceEndpoint.publish("http://localhost:" + PORT + "/orchard/json/executor");
 	}
 }
