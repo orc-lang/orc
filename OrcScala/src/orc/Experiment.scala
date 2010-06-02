a3441809edaa56384b1a817bf589f958db3472d5
John A. Thywissen <jthywiss@users.noreply.github.com>
2010-06-02 04:59:07 +0000

    Lexical scanner understands Orc literals

--- OrcScala/src/orc/Experiment.scala
+++ OrcScala/src/orc/Experiment.scala
@@ -48,7 +48,7 @@ object Experiment {
 //      )
 //  )
 
-  val parseTest = "5 >> 3 | (7 | 8) >x> (x | x) | (if true then 1111 else 9999)"
+  val parseTest = "5 >> 3 | (7 | 8) >x> (x | x) | (if true then -8.8e+8 else 9999)"
 
   def main(args: Array[String]) {
     val parsedOil = (new OrcCompiler())(new StringReader(parseTest), ExperimentOptions)
