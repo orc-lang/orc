b8e62239cdbd2900bdabe0f3f7fc97d95a15bac7
John A. Thywissen <jthywiss@users.noreply.github.com>
2010-06-03 15:12:53 +0000

    Set svn:keywords

--- OrcScala/src/orc/sites/compatibility/type/structured/EllipsisArrowType.java
+++ OrcScala/src/orc/sites/compatibility/type/structured/EllipsisArrowType.java
@@ -2,7 +2,7 @@
 // EllipsisArrowType.java -- Java class EllipsisArrowType
 // Project OrcJava
 //
-// $Id: EllipsisArrowType.java 1502 2010-02-03 06:25:53Z jthywissen $
+// $Id$
 //
 // Copyright (c) 2009 The University of Texas at Austin. All rights reserved.
 //
--- OrcScala/src/orc/sites/compatibility/type/structured/ListType.java
+++ OrcScala/src/orc/sites/compatibility/type/structured/ListType.java
@@ -2,7 +2,7 @@
 // ListType.java -- Java class ListType
 // Project OrcJava
 //
-// $Id: ListType.java 1553 2010-02-26 17:02:05Z jthywissen $
+// $Id$
 //
 // Copyright (c) 2009 The University of Texas at Austin. All rights reserved.
 //
