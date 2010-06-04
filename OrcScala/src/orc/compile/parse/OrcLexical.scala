b926b013da2d0be2d607ac339efa51e62a9c97f7
Amin Shali <amshali@gmail.com>
2010-06-04 19:47:53 +0000

    Added ParserNL for recognizing the newlines. It's not incorporated in the OrcParser yet.

--- OrcScala/src/orc/compile/parse/OrcLexical.scala
+++ OrcScala/src/orc/compile/parse/OrcLexical.scala
@@ -20,34 +20,39 @@ import scala.util.parsing.input.CharArrayReader.EofCh
 import scala.collection.mutable.HashSet
 
 /**
- * 
+ *
  *
  * @author jthywiss
  */
 
 class OrcLexical() extends StdLexical() {
-  
+
   case class FloatingPointLit(chars: String) extends Token {
     override def toString = chars
   }
 
-  override def token: Parser[Token] = 
+  case object NewLine extends Token {
+    def chars = "\n"
+  }
+
+  override def token: Parser[Token] =
     ( identChar ~ rep( identChar | digit )              ^^ { case first ~ rest => processIdent(first :: rest mkString "") }
     | '(' ~ delim ~ ')'                                 ^^ { case '(' ~ d ~ ')' => Identifier(d.chars) }
     | '(' ~ '0' ~ '-' ~ ')'                             ^^ { _ => Identifier("0-") }
     | delim
     | '\"' ~ rep( chrExcept('\"', '\n', EofCh) ) ~ '\"' ^^ { case '\"' ~ chars ~ '\"' => StringLit(chars mkString "") }
-    | '\"' ~> failure("unclosed string literal")        
+    | '\"' ~> failure("unclosed string literal")
     | floatLit                                          ^^ { case f => FloatingPointLit(f) }
     | signedIntegerLit                                  ^^ { case i => NumericLit(i) }
     | EofCh                                             ^^^ EOF
+    | '\n'                                              ^^^ NewLine
     | failure("illegal character")
     )
-  
+
   // legal identifier chars other than digits
   override def identChar = letter | elem('_') | elem('\'')
 
-  def nonZeroDigit = elem('1') | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'  
+  def nonZeroDigit = elem('1') | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
   def integerLit =
       ( elem('0')                 ^^ { _.toString() }
       | rep1(nonZeroDigit, digit) ^^ { _ mkString "" }
@@ -59,14 +64,14 @@ class OrcLexical() extends StdLexical() {
   def plusMinusIntegerLit =
       ( elem('+') ~ integerLit ^^ { case a ~ b => a+b }
       | signedIntegerLit )
-  
-  def floatLit = 
+
+  def floatLit =
     ( signedIntegerLit ~ '.' ~ integerLit ~ (elem('e') |  elem('E')) ~ plusMinusIntegerLit ^^ { case a ~ b ~ c ~ d ~ e => a+b+c+d+e }
     | signedIntegerLit                    ~ (elem('e') |  elem('E')) ~ plusMinusIntegerLit ^^ { case a ~ b ~ c => a+b+c }
     | signedIntegerLit ~ '.' ~ integerLit                                                  ^^ { case a ~ b ~ c => a+b+c }
     )
-  
-  override def whitespaceChar = elem("space char", ch => ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r' || ch == '\f')
+
+  override def whitespaceChar = elem("space char", ch => ch == ' ' || ch == '\t' || ch == '\r' || ch == '\f')
 
   override def whitespace: Parser[Any] = rep(
       whitespaceChar
