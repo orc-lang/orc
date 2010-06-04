6a0831d4b7359fa089dc166a3585e4b6b25f086c
jthywissen <jthywissen@440195e8-269f-11de-a2b9-47ed1c40409f>
2010-06-04 00:11:48 +0000

    Add file header comment blocks

--- trunk/OrcScala/src/orc/compile/translate/AggregateDef.scala
+++ trunk/OrcScala/src/orc/compile/translate/AggregateDef.scala
@@ -1,3 +1,18 @@
+//
+// AggregateDef.scala -- Scala class AggregateDef
+// Project OrcScala
+//
+// $Id$
+//
+// Created by dkitchin on Jun 3, 2010.
+//
+// Copyright (c) 2010 The University of Texas at Austin. All rights reserved.
+//
+// Use and redistribution of this file is governed by the license terms in
+// the LICENSE file found in the project's top-level directory and also found at
+// URL: http://orc.csres.utexas.edu/license.shtml .
+//
+
 package orc.compile.translate
 
 import orc.PartialMapExtension._
--- trunk/OrcScala/src/orc/oil/nameless/NamelessInfixCombinators.scala
+++ trunk/OrcScala/src/orc/oil/nameless/NamelessInfixCombinators.scala
@@ -1,3 +1,18 @@
+//
+// NamelessInfixCombinators.scala -- Scala trait NamelessInfixCombinators
+// Project OrcScala
+//
+// $Id$
+//
+// Created by dkitchin on May 31, 2010.
+//
+// Copyright (c) 2010 The University of Texas at Austin. All rights reserved.
+//
+// Use and redistribution of this file is governed by the license terms in
+// the LICENSE file found in the project's top-level directory and also found at
+// URL: http://orc.csres.utexas.edu/license.shtml .
+//
+
 package orc.oil.nameless
 
 // Infix combinator constructors
