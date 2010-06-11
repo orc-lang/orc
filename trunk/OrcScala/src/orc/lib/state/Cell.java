27a5cabf30c43c1e11ff9b7878421c454fc07744
jthywissen <jthywissen@440195e8-269f-11de-a2b9-47ed1c40409f>
2010-06-11 04:36:26 +0000

    Set svn:keywords

--- trunk/OrcScala/src/orc/lib/state/Cell.java
+++ trunk/OrcScala/src/orc/lib/state/Cell.java
@@ -2,7 +2,7 @@
 // Cell.java -- Java class Cell
 // Project OrcJava
 //
-// $Id: Cell.java 1502 2010-02-03 06:25:53Z jthywissen $
+// $Id$
 //
 // Copyright (c) 2009 The University of Texas at Austin. All rights reserved.
 //
