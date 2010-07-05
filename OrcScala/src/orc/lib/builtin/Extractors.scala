50f4abe81e11a5b17331f2cfc498bc004142e886
Amin Shali <amshali@gmail.com>
2010-07-05 17:50:30 +0000

    Fixed the problem with list extractors and java list. Added a new example.

--- OrcScala/src/orc/lib/builtin/Extractors.scala
+++ OrcScala/src/orc/lib/builtin/Extractors.scala
@@ -32,14 +32,19 @@ object SomeExtractor extends PartialSite with UntypedSite {
 
 object NilExtractor extends PartialSite with UntypedSite {
   override def name = "Nil?"
-  def evaluate(args: List[Value]) =
+  def evaluate(args: List[Value]) = {
     args match {
       case List(OrcList(Nil)) => Some(Signal)
       case List(OrcList(_)) => None
+      case List(Literal(OrcList(Nil))) => Some(Signal)
+      case List(Literal(OrcList(_))) => None
       case List(Literal(arr: Array[AnyRef])) if (arr.size == 0) => Some(Signal)
       case List(Literal(arr: Array[AnyRef])) if (arr.size != 0) => None
+      case List(Literal(c:java.util.List[Any])) if (c.size == 0) => Some(Signal)
+      case List(Literal(c:java.util.List[Any])) if (c.size != 0) => None
       case List(a) => throw new ArgumentTypeMismatchException(0, "List", a.getClass().toString())
       case _ => throw new ArityMismatchException(1, args.size)
+  }
   }
 }
 
@@ -49,10 +54,16 @@ object ConsExtractor extends PartialSite with UntypedSite {
     args match {
       case List(OrcList(v :: vs)) => Some(OrcTuple(List(v, OrcList(vs))))
       case List(OrcList(_)) => None
+      case List(Literal(OrcList(v :: vs))) => Some(OrcTuple(List(v, OrcList(vs))))
+      case List(Literal(OrcList(_))) => None
+      case List(Literal(c:java.util.List[Any])) if (c.size != 0) => {
+        Some(OrcTuple(List(Literal(c.get(0)),Literal(c.subList(1,c.size)))))
+      }
+      case List(Literal(c:java.util.List[Any])) if (c.size == 0) => None
       case List(Literal(arr: Array[AnyRef])) if (arr.size != 0) => { // Allow List-like pattern matching on arrays.
         Some(OrcTuple(List(Literal(arr(0)),Literal(arr.slice(1,arr.size)))))
       }
-      case List(Literal(arr: Array[AnyRef])) if (arr.size != 0) => None
+      case List(Literal(arr: Array[AnyRef])) if (arr.size == 0) => None
       case List(a) => throw new ArgumentTypeMismatchException(0, "List", a.getClass().toString())
       case _ => throw new ArityMismatchException(1, args.size)
   }
