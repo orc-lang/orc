49c849ebee1dfd20924585b3716374a1c5e378e0
John A. Thywissen <jthywiss@users.noreply.github.com>
2010-07-09 22:35:36 +0000

    Set svn:keywords

--- OrcScala/src/orc/values/sites/compatibility/ArrayProxy.java
+++ OrcScala/src/orc/values/sites/compatibility/ArrayProxy.java
@@ -2,7 +2,7 @@
 // ArrayProxy.java -- Java class ArrayProxy
 // Project OrcJava
 //
-// $Id: ArrayProxy.java 1502 2010-02-03 06:25:53Z jthywissen $
+// $Id$
 //
 // Copyright (c) 2009 The University of Texas at Austin. All rights reserved.
 //
