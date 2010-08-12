de163872abbc6f2c45f5f1dfeb8e70699de77580
jthywissen <jthywissen@440195e8-269f-11de-a2b9-47ed1c40409f>
2010-08-12 21:12:30 +0000

    Add severity info to CompilationExceptions

--- trunk/OrcScala/src/orc/error/compiletime/CompilationExceptions.scala
+++ trunk/OrcScala/src/orc/error/compiletime/CompilationExceptions.scala
@@ -16,13 +16,32 @@ package orc.error.compiletime
 
 import scala.util.parsing.input.Position
 
+import orc.error.compiletime.CompileLogger.Severity
+
+trait CompilationExceptionSeverity { }
+
+/** Severity of this exception is internal, for tool debugging -- users don't care. */
+trait SeverityDebug { val severity = Severity.DEBUG }
+/** Severity of this exception is completely routine.  For example, counts of output size. */
+trait SeverityInfo { val severity = Severity.INFO }
+/** Severity of this exception is not routine, but not a problem. */
+trait SeverityNotice { val severity = Severity.NOTICE }
+/** Severity of this exception is a potential problem, but not bad enough to cause output to be disregarded -- it may still be usable. */
+trait SeverityWarning { val severity = Severity.WARNING }
+/** Severity of this exception is a problem that is severe enough that output was discarded or should be discarded -- it is not usable. */
+trait SeverityError{ val severity = Severity.ERROR }
+/** Severity of this exception is a problem that has caused input processing to be stopped. */
+trait SeverityFatal { val severity = Severity.FATAL }
+/** Severity of this exception is an internal failure of the tool (not the user's fault). */
+trait SeverityInternal{ val severity = Severity.INTERNAL }
+
 /**
  * Problem parsing the text of an Orc program. Mostly this
  * is a wrapper around the exceptions thrown by whatever
  * parsing library we use.
  */
 class ParsingException(message: String, errorPos: Position) extends
-  CompilationException(message) {
+  CompilationException(message) with SeverityError {
   setPosition(errorPos)
 }
 
@@ -30,7 +49,7 @@ class ParsingException(message: String, errorPos: Position) extends
  * 
  */
 class PatternException(message: String) extends
-  CompilationException(message)
+  CompilationException(message) with SeverityError
 
 /**
  * Indicate a problem with site resolution. Ideally
@@ -38,10 +57,10 @@ class PatternException(message: String) extends
  * resolution is done at runtime.
  */
 class SiteResolutionException(val siteName: String, cause: Throwable) extends
-  CompilationException("Problem loading site "+siteName, cause)
+  CompilationException("Problem loading site "+siteName, cause) with SeverityFatal
 
 /**
  * 
  */
 class UnboundVariableException(val varName: String) extends
-  CompilationException("Variable " + varName + " is unbound")
+  CompilationException("Variable " + varName + " is unbound") with SeverityError
--- trunk/OrcScala/src/orc/error/compiletime/typing/TypeExceptions.scala
+++ trunk/OrcScala/src/orc/error/compiletime/typing/TypeExceptions.scala
@@ -15,39 +15,40 @@
 package orc.error.compiletime.typing
 
 import orc.types.Type
+import orc.error.compiletime._
 
 class ArgumentArityException(val arityExpected: Int, val arityReceived: Int) extends
-  TypeException("Expected " + arityExpected + " arguments to call, got " + arityReceived + " arguments instead.")
+  TypeException("Expected " + arityExpected + " arguments to call, got " + arityReceived + " arguments instead.") with SeverityError
 
 class DefinitionArityException(val arityFromType: Int, val arityFromSyntax: Int) extends
-  TypeException("Definition should have " + arityFromType + " arguments according to its type, observed " + arityFromSyntax + " arguments instead.")
+  TypeException("Definition should have " + arityFromType + " arguments according to its type, observed " + arityFromSyntax + " arguments instead.") with SeverityError
 
 class MissingTypeException() extends
-  TypeException("Type checker failed: couldn't obtain sufficient type information from a service or value.")
+  TypeException("Type checker failed: couldn't obtain sufficient type information from a service or value.") with SeverityError
 
 class MultiTypeException(val specificMessages: String) extends
-  TypeException("All alternatives for multitype failed to typecheck.\n" + specificMessages) {
+  TypeException("All alternatives for multitype failed to typecheck.\n" + specificMessages) with SeverityError {
   def addAlternative(t: Type, e: TypeException) =
     new MultiTypeException(specificMessages + (t + " failed due to error: \n" + e + "\n"));
 }
 
 class SubtypeFailureException(val expectedType: Type, val receivedType: Type) extends
-  TypeException("Expected type " + expectedType + " or some subtype, found type " + receivedType + " instead.")
+  TypeException("Expected type " + expectedType + " or some subtype, found type " + receivedType + " instead.") with SeverityError
 
 class TypeArityException(val arityExpected: Int, val arityReceived: Int) extends
-  TypeException("Expected " + arityExpected + " arguments to type instantiation, got " + arityReceived + " arguments instead.")
+  TypeException("Expected " + arityExpected + " arguments to type instantiation, got " + arityReceived + " arguments instead.") with SeverityError
 
 class UnboundTypeException(val typeName: String) extends
-  TypeException("Type " + typeName + " is undefined")
+  TypeException("Type " + typeName + " is undefined") with SeverityError
 
 class UncallableTypeException(val t: Type) extends
-  TypeException("Type " + t + " cannot be called as a service or function.")
+  TypeException("Type " + t + " cannot be called as a service or function.") with SeverityError
 
 class UnrepresentableTypeException(val t: Type) extends
-  TypeException(t.toString() + " has no concrete syntax.")
+  TypeException(t.toString() + " has no concrete syntax.") with SeverityError
 
 class UnspecifiedArgTypesException() extends
-  TypeException("Could not perform type check due to missing argument types; please add argument type annotations")
+  TypeException("Could not perform type check due to missing argument types; please add argument type annotations") with SeverityError
 
 class UnspecifiedReturnTypeException() extends
-  TypeException("Could not perform type check due to missing return type; please add a return type annotation");
+  TypeException("Could not perform type check due to missing return type; please add a return type annotation") with SeverityError
