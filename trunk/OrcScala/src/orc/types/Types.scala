6fcab0934df0cf0821904103c454869e456d0518
jthywissen <jthywissen@440195e8-269f-11de-a2b9-47ed1c40409f>
2010-08-17 16:41:08 +0000

    Eliminate redundant SubtypeFailureException declaration

--- trunk/OrcScala/src/orc/types/Types.scala
+++ trunk/OrcScala/src/orc/types/Types.scala
@@ -15,52 +15,50 @@
 
 package orc.types
 
-import orc.error.compiletime.typing._
-
-class SubtypeFailureException(t1: Type, t2: Type) extends Throwable
+import orc.error.compiletime.typing.SubtypeFailureException
 
 abstract class Type {
-  def join(that: Type) : Type
-  def meet(that: Type) : Type
+  def join(that: Type): Type
+  def meet(that: Type): Type
   def assertSubtype(that: Type)
 }
 
 object TypeConversions {
-  implicit def type2type(that : orc.oil.nameless.Type) : Type = that match {
+  implicit def type2type(that: orc.oil.nameless.Type): Type = that match {
     case orc.oil.nameless.Top() => Top
     case orc.oil.nameless.Bot() => Bot
     case orc.oil.nameless.FunctionType(a, p, r) => ArrowType(a, p map type2type, r)
     case orc.oil.nameless.TypeVar(i) => TypeVar(i)
   }
-  implicit def typelist2typelist(that : List[orc.oil.nameless.Type]) : List[Type] = that map type2type
+  implicit def typelist2typelist(that: List[orc.oil.nameless.Type]): List[Type] = that map type2type
 }
 
 case object Top extends Type {
-  def join(that: Type) : Type = this
-  def meet(that: Type) : Type = that
+  def join(that: Type): Type = this
+  def meet(that: Type): Type = that
   def assertSubtype(that: Type) {
     that match {
-      case Top => { }
+      case Top => {}
       case _ => throw new SubtypeFailureException(this, that) //FIXME: Supply source location 
     }
   }
 }
 
 case object Bot extends Type {
-  def join(that: Type) : Type = that
-  def meet(that: Type) : Type = this
-  def assertSubtype(that: Type) { }
+  def join(that: Type): Type = that
+  def meet(that: Type): Type = this
+  def assertSubtype(that: Type) {}
 }
 
 case class ArrowType(typeFormalArity: Int, argTypes: List[Type], returnType: Type) extends Type {
-  def join(that: Type) : Type = that match {
+  def join(that: Type): Type = that match {
     case ArrowType(thatTypeFormalArity, thatargTypes, thatReturnType) if (sameShape(that)) => {
       val combinedargTypes = for ((t1, t2) <- argTypes zip thatargTypes) yield t1 meet t2
       ArrowType(thatTypeFormalArity, combinedargTypes, returnType join thatReturnType)
     }
     case _ => Top
   }
-  def meet(that: Type) : Type = that match {
+  def meet(that: Type): Type = that match {
     case ArrowType(thatTypeFormalArity, thatargTypes, thatReturnType) if (sameShape(that)) => {
       val combinedargTypes = for ((t1, t2) <- argTypes zip thatargTypes) yield t1 join t2
       ArrowType(thatTypeFormalArity, combinedargTypes, returnType meet thatReturnType)
@@ -73,29 +71,29 @@ case class ArrowType(typeFormalArity: Int, argTypes: List[Type], returnType: Typ
         val combinedargTypes = for ((t1, t2) <- argTypes zip thatargTypes) yield t1 meet t2
         returnType assertSubtype thatReturnType
       }
-      case Top => { }
+      case Top => {}
       case _ => throw new SubtypeFailureException(this, that) //FIXME: Supply source location
     }
   }
-  def sameShape(that : Type) : Boolean = that match {
+  def sameShape(that: Type): Boolean = that match {
     case ArrowType(thatTypeFormalArity, thatargTypes, thatReturnType) => (typeFormalArity == thatTypeFormalArity) && (argTypes.size == thatargTypes.size)
     case _ => false
   }
 }
 
 case class TypeVar(index: Int) extends Type {
-  def join(that: Type) : Type = that match {
+  def join(that: Type): Type = that match {
     case TypeVar(thatIndex) if (index == thatIndex) => this
     case _ => Top
   }
-  def meet(that: Type) : Type = that match {
+  def meet(that: Type): Type = that match {
     case TypeVar(thatIndex) if (index == thatIndex) => this
     case _ => Bot
   }
   def assertSubtype(that: Type) {
     that match {
-      case TypeVar(thatIndex) if (index == thatIndex) => { }
-      case Top => { }
+      case TypeVar(thatIndex) if (index == thatIndex) => {}
+      case Top => {}
       case _ => throw new SubtypeFailureException(this, that) //FIXME: Supply source location
     }
   }
