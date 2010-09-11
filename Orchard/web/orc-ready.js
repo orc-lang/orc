70be8318a794a7226412ae78f22847432cb2a1a1
John A. Thywissen <jthywiss@users.noreply.github.com>
2010-09-11 21:08:06 +0000

    Add file header comments

--- Orchard/web/orc-ready.js
+++ Orchard/web/orc-ready.js
@@ -1,3 +1,16 @@
+//
+// orc-ready.js -- JavaScript source for the "Try Orc" Orchard Web interface
+// Project Orchard
+//
+// $Id$
+//
+// Copyright (c) 2010 The University of Texas at Austin. All rights reserved.
+//
+// Use and redistribution of this file is governed by the license terms in
+// the LICENSE file found in the project's top-level directory and also found at
+// URL: http://orc.csres.utexas.edu/license.shtml .
+//
+
 /**
  * This is loaded automatically by orc.js after jQuery.
  *
