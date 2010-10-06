379e36f5d71a8f476d0a9de56340d739ce858d69
dkitchin <dkitchin@440195e8-269f-11de-a2b9-47ed1c40409f>
2010-10-06 08:12:37 +0000

    Terminated Orca branch of OrcJava. Orca will subsequently branch from OrcScala.

--- branches/Orca/src/orc/ast/oil/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Internal representation of OIL, the Orc Intermediate Language.
- * This is an AST which uses DeBruijn indices and therefore
- * has no explicit named variables. This is the simplest AST representation
- * of a program before it is translated into a runtime DAG.
- */
-
-package orc.ast.oil;
