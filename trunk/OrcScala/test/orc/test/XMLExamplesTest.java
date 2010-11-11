76dd899bb5373ea6b7e1436164770ebf7b6abf65
dkitchin <dkitchin@440195e8-269f-11de-a2b9-47ed1c40409f>
2010-11-11 12:58:45 +0000

    Overhaul of XML output/input. Created an XSD file for schema validation of oil XML. XML examples test now includes a validation step. Fixed a parsing error on types.

--- trunk/OrcScala/test/orc/test/XMLExamplesTest.java
+++ trunk/OrcScala/test/orc/test/XMLExamplesTest.java
@@ -24,19 +24,25 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import orc.error.OrcException;
 import orc.ast.oil.nameless.Expression;
-import orc.ast.oil.nameless.OrcXML;
+import orc.ast.oil.xml.OrcXML;
 import orc.script.OrcScriptEngine;
+
+import javax.xml.validation.*;
+import javax.xml.transform.stream.StreamSource;
+
+import org.xml.sax.SAXException;
 
 /**
  * Test Orc by running annotated sample programs from the "examples" directory.
- * Each program is compiled, written to XML, then read back as an AST. This
- * second AST is run. Each program is given at most 10 seconds to complete.
+ * Each program is compiled, written to XML, subjected to validation against
+ * an XML schema, and then read back as an AST. This second AST is run. 
+ * Each program is given at most 10 seconds to complete.
  * <p>
  * We look for one or more comment blocks formatted per
  * <code>ExampleOutput</code>'s specs.
  * 
  * @see ExpectedOutput
- * @author quark, srosario, amshali
+ * @author quark, srosario, amshali, dkitchin
  */
 public class XMLExamplesTest {
 	public static Test suite() {
@@ -64,8 +70,35 @@ public class XMLExamplesTest {
 					System.out.println("\n==== Starting " + file + " ====");
 					final OrcScriptEngine.OrcCompiledScript compiledScript = OrcForTesting.compile(file.getPath());
 					final Expression expr = getAstRoot(compiledScript);
-					final Expression exprXML = orc.ast.oil.nameless.OrcXML.fromXML(OrcXML.writeXML(expr));
-					setAstRoot(compiledScript, exprXML);
+					
+					// AST -> XML
+					final scala.xml.Elem xmlFromExpr = OrcXML.astToXml(expr);
+					
+					 
+					// Locate .xsd file resource
+					final ClassLoader clX = Thread.currentThread().getContextClassLoader();
+					final ClassLoader clY = getClass().getClassLoader();
+					final ClassLoader clZ = ClassLoader.getSystemClassLoader();
+					final ClassLoader classLoader = clX != null ? clX : (clY != null ? clY : clZ);
+					final java.io.InputStream xsdStream = classLoader.getResource("orc/ast/oil/xml/orc.xsd").openStream();
+					
+					// Schema validation
+					try {
+				      final SchemaFactory schemaFactory = SchemaFactory.newInstance(javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI);
+	                  final Schema schema = schemaFactory.newSchema(new StreamSource(xsdStream));
+	                  final Validator validator = schema.newValidator();
+	                  final StreamSource xmlsource = new StreamSource(new java.io.StringReader(xmlFromExpr.toString()));
+	                  validator.validate(xmlsource);
+                    } 
+				    catch (SAXException e) {
+				      throw new AssertionError("XML validation failed: " + e.getMessage());
+                    }
+									    
+					// XML -> AST
+					final Expression exprFromXml = OrcXML.xmlToAst(xmlFromExpr);
+					
+					// Execution
+					setAstRoot(compiledScript, exprFromXml);
 					final String actual = OrcForTesting.run(compiledScript, 10L);
 					if (!expecteds.contains(actual)) {
 						throw new AssertionError("Unexpected output:\n" + actual);
