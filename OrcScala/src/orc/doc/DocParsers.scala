0fa7d24f7c50816860f9d09780507495b8640c28
David Kitchin <dkitchin@gmail.com>
2010-12-18 04:06:08 +0000

    Skeleton of docmaker implementation

---
+++ OrcScala/src/orc/doc/DocParsers.scala
@@ -0,0 +1,31 @@
+//
+// DocParsers.scala -- Scala class/trait/object DocParsers
+// Project OrcScala
+//
+// $Id$
+//
+// Created by dkitchin on Dec 16, 2010.
+//
+// Copyright (c) 2010 The University of Texas at Austin. All rights reserved.
+//
+// Use and redistribution of this file is governed by the license terms in
+// the LICENSE file found in the project's top-level directory and also found at
+// URL: http://orc.csres.utexas.edu/license.shtml .
+//
+package orc.doc
+
+import scala.util.parsing.combinator.RegexParsers
+
+/**
+ * 
+ *
+ * @author dkitchin
+ */
+object DocParsers extends RegexParsers {
+
+  
+  
+  
+  
+}
+
