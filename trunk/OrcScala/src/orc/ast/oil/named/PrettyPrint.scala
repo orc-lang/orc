c604087f05c8bb2f78e64b5f9fd67d75a38bd082
jthywissen <jthywissen@440195e8-269f-11de-a2b9-47ed1c40409f>
2011-04-20 01:12:36 +0000

    Update copyright dates & add license file to projects that were missing it

--- trunk/OrcScala/src/orc/ast/oil/named/PrettyPrint.scala
+++ trunk/OrcScala/src/orc/ast/oil/named/PrettyPrint.scala
@@ -6,7 +6,7 @@
 //
 // Created by dkitchin on Jun 7, 2010.
 //
-// Copyright (c) 2010 The University of Texas at Austin. All rights reserved.
+// Copyright (c) 2011 The University of Texas at Austin. All rights reserved.
 //
 // Use and redistribution of this file is governed by the license terms in
 // the LICENSE file found in the project's top-level directory and also found at
