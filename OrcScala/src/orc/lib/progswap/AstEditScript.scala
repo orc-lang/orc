a28bbbec3f5689eeddef5730aa993d05c6739a46
John A. Thywissen <jthywiss@users.noreply.github.com>
2011-05-31 13:52:09 +0000

    Snapshot of ProgSwap work. Status: - LCS implemented and tested - AST match implemented and lightly tested - Edit script generation implemented and buggy - Move to new AST partially implemented/stubbed and buggy

--- OrcScala/src/orc/lib/progswap/AstEditScript.scala
+++ OrcScala/src/orc/lib/progswap/AstEditScript.scala
@@ -15,6 +15,7 @@
 package orc.lib.progswap
 
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.MutableList
 import orc.ast.oil.nameless.NamelessAST
 
 /**
@@ -26,38 +27,43 @@ class AstEditScript extends ArrayBuffer[AstEditOperation] {
 
 }
 
+/**
+ * 
+ *
+ * @author jthywiss
+ */
 object AstEditScript {
 
-  /**
-   * Create an AstEditScript describing the operations necessary to
-   * modify <code>oldOilAst</code> into <code>newOilAst</code>.
-   *
-   * @param oldOilAst
-   * @param newOilAst
-   * @return the computed AstEditScript
-   */
-  def computeEditScript(oldOilAst: NamelessAST, newOilAst: NamelessAST): AstEditScript = {
-      val editScript = new AstEditScript()
-      r(editScript, oldOilAst, newOilAst)
-      editScript
-  }
-
-  /**
-   * Mindless test implementation -- for two OIL ASTs of the same shape,
-   * traverse both in lock-step and replace each old node with the 
-   * corresponding new node.
-   */
-  def r(script: AstEditScript, oldNode: NamelessAST, newNode: NamelessAST): AstEditScript = {
-    script += new ReplaceNode(oldNode, newNode)
-    val oldIter = oldNode.subtrees.iterator
-    val newIter = newNode.subtrees.iterator
-    while (oldIter.hasNext && newIter.hasNext) {
-      r(script, oldIter.next(), newIter.next());
-    }
-    if (oldIter.hasNext || newIter.hasNext) {
-      throw new AssertionError("ASTs are not the same shape");
-    }
-    script
-  }
+//  /**
+//   * Create an AstEditScript describing the operations necessary to
+//   * modify <code>oldOilAst</code> into <code>newOilAst</code>.
+//   *
+//   * @param oldOilAst
+//   * @param newOilAst
+//   * @return the computed AstEditScript
+//   */
+//  def computeEditScript(oldOilAst: NamelessAST, newOilAst: NamelessAST): AstEditScript = {
+//      val editScript = new AstEditScript()
+//      r(editScript, oldOilAst, newOilAst)
+//      editScript
+//  }
+//
+//  /**
+//   * Mindless test implementation -- for two OIL ASTs of the same shape,
+//   * traverse both in lock-step and replace each old node with the 
+//   * corresponding new node.
+//   */
+//  def r(script: AstEditScript, oldNode: NamelessAST, newNode: NamelessAST): AstEditScript = {
+//    script += new ReplaceNode(oldNode, newNode)
+//    val oldIter = oldNode.subtrees.iterator
+//    val newIter = newNode.subtrees.iterator
+//    while (oldIter.hasNext && newIter.hasNext) {
+//      r(script, oldIter.next(), newIter.next());
+//    }
+//    if (oldIter.hasNext || newIter.hasNext) {
+//      throw new AssertionError("ASTs are not the same shape");
+//    }
+//    script
+//  }
 
 }
