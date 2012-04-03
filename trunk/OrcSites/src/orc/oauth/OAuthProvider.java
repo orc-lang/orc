ccafc5d060093d1ecd8c3174273975ec8e26fb42
jthywissen <jthywissen@440195e8-269f-11de-a2b9-47ed1c40409f>
2012-04-03 02:16:30 +0000

    Hack the OAuth stuff back into some semblance of working.

--- trunk/OrcSites/src/orc/oauth/OAuthProvider.java
+++ trunk/OrcSites/src/orc/oauth/OAuthProvider.java
@@ -4,7 +4,7 @@
 //
 // $Id$
 //
-// Copyright (c) 2009 The University of Texas at Austin. All rights reserved.
+// Copyright (c) 2012 The University of Texas at Austin. All rights reserved.
 //
 // Use and redistribution of this file is governed by the license terms in
 // the LICENSE file found in the project's top-level directory and also found at
@@ -15,11 +15,9 @@ package orc.oauth;
 
 import java.io.IOException;
 import java.security.PrivateKey;
-import java.util.List;
 
-import net.oauth.OAuth;
-import net.oauth.OAuthAccessor;
-import net.oauth.OAuthConsumer;
+import orc.values.sites.compatibility.DotSite;
+
 import net.oauth.OAuthException;
 import net.oauth.signature.RSA_SHA1;
 
@@ -30,32 +28,32 @@ import net.oauth.signature.RSA_SHA1;
  * 
  * @author quark
  */
-public abstract class OAuthProvider {
+public abstract class OAuthProvider extends DotSite {
 	protected SimpleOAuth oauth;
 
 	public OAuthProvider(final String properties) throws IOException {
 		oauth = new SimpleOAuth(properties);
 	}
 
-	public OAuthAccessor authenticate(final String name) throws Exception {
-		return authenticate(name, OAuth.newList());
-	}
+//	public OAuthAccessor authenticate(final String name) throws Exception {
+//		return authenticate(name, OAuth.newList());
+//	}
 
-	/**
-	 * Get an authenticated OAuthAccessor.
-	 * 
-	 * @param name
-	 * @param request
-	 * @return
-	 * @throws Exception
-	 */
-	public OAuthAccessor authenticate(final String name, final List<OAuth.Parameter> request) throws Exception {
-		throw new AssertionError("Must override OAuthProvider#authenticate(String)");
-	}
+//	/**
+//	 * Get an authenticated OAuthAccessor.
+//	 * 
+//	 * @param name
+//	 * @param request
+//	 * @return
+//	 * @throws Exception
+//	 */
+//	public OAuthAccessor authenticate(final String name, final List<OAuth.Parameter> request) throws Exception {
+//		throw new AssertionError("Must override OAuthProvider#authenticate(String)");
+//	}
 
-	public final OAuthConsumer getConsumer(final String name) throws OAuthException {
-		return oauth.getConsumer(name);
-	}
+//	public final OAuthConsumer getConsumer(final String name) throws OAuthException {
+//		return oauth.getConsumer(name);
+//	}
 
 	public final PrivateKey getPrivateKey(final String consumer) throws OAuthException {
 		return (PrivateKey) oauth.getConsumer(consumer).getProperty(RSA_SHA1.PRIVATE_KEY);
