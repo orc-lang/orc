53501c512b80c3b7175224642743c5379ad3ec14
jthywissen <jthywissen@440195e8-269f-11de-a2b9-47ed1c40409f>
2013-01-13 21:52:51 +0000

    HTTP site: halt call handle when exception thrown; other tiny robustness improvements

--- trunk/OrcScala/src/orc/lib/web/HTTP.scala
+++ trunk/OrcScala/src/orc/lib/web/HTTP.scala
@@ -14,17 +14,16 @@
 //
 package orc.lib.web
 
-import orc.values.sites.{ TotalSite, Site0, Site1, Site2 }
+import java.io.OutputStreamWriter
+import java.net.{ URLEncoder, URL }
+
+import scala.Array.canBuildFrom
+import scala.io.Source
+
+import orc.error.runtime.{ SiteException, JavaException }
+import orc.values.sites.{ TotalSite, Site2, Site0 }
 import orc.values.OrcRecord
 import orc.Handle
-import orc.error.runtime.SiteException
-
-import java.net.{ URLConnection, URL, URLEncoder }
-import java.io.{ OutputStreamWriter, InputStreamReader }
-import java.lang.StringBuilder
-import java.net.URL
-
-import scala.io.Source
 
 /** The HTTP site provides a simple mechanism to send GET and POST requests to a URL.
   *
@@ -51,6 +50,7 @@ object HTTP extends TotalSite {
   def convertToQueryPairs(entries: Map[String, AnyRef]): String = {
     val nameValuePairs =
       for ((name, v) <- entries; value = v.toString()) yield {
+        /* Always use UTF-8, per RFC 3986 section 2.5 */
         URLEncoder.encode(name, "UTF-8") + "=" + URLEncoder.encode(value, "UTF-8")
       }
     nameValuePairs reduceLeft { _ + "&" + _ }
@@ -64,36 +64,43 @@ object HTTP extends TotalSite {
   }
 
   private def charEncodingFromContentType(contentType: java.lang.String): java.lang.String = {
+    if (contentType == null) return "ISO-8859-1" /* default per RFC 2616 section 3.7.1 */
     //TODO: Don't break if quoted parameter values are used (see RFC 2045 section 5.1)
     val contentTypeParams = contentType.split(";").map(_.trim()).toList.tail
     val charsetValues = List("ISO-8859-1") /* default per RFC 2616 section 3.7.1 */ ++ (
       for (param <- contentTypeParams if param.toLowerCase.startsWith("charset="))
-        yield param.substring(8)
-      )
+        yield param.substring(8))
     charsetValues.last
   }
 
   lazy val userAgent = "Orc/" + orc.Main.versionProperties.getProperty("orc.version") +
-      " Java/" + java.lang.System.getProperty("java.version")
+    " Java/" + java.lang.System.getProperty("java.version")
 
   case class HTTPGet(url: URL) extends Site0 {
     def call(h: Handle) {
       val getAction =
         new Runnable {
           def run() {
-            val conn = url.openConnection
-            conn.setConnectTimeout(10000)
-            conn.setReadTimeout(5000)
-            conn.setRequestProperty("User-Agent", userAgent)
-            conn.connect()
+            try {
+              val conn = url.openConnection
+              conn.setConnectTimeout(10000)
+              conn.setReadTimeout(5000)
+              conn.setRequestProperty("User-Agent", userAgent)
+              conn.connect()
+              //TODO: Make response code available to caller
+              val contentType = conn.getContentType()
+              //TODO: Confirm our assumption that the content is a character stream
+              val charEncoding = charEncodingFromContentType(contentType)
 
-            val contentType = conn.getContentType()
-            //TODO: Confirm our assumption that the content is a character stream
-            val charEncoding = charEncodingFromContentType(contentType)
-            val in = Source.fromInputStream(conn.getInputStream, charEncoding)
-            val result = in.mkString
-            in.close
-            h.publish(result)
+              val in = Source.fromInputStream(conn.getInputStream, charEncoding)
+              val result = in.mkString
+              in.close
+
+              h.publish(result)
+            } catch {
+              case ie: InterruptedException => { h.halt; throw ie }
+              case e: Exception => { h !! new JavaException(e) }
+            }
           }
         }
       (new Thread(getAction)).start()
@@ -104,32 +111,38 @@ object HTTP extends TotalSite {
     def call(a: AnyRef, b: AnyRef, h: Handle) {
       val post = a.toString
       val postContentType = b.toString +
-          (if (b.toString.toLowerCase.contains("charset="))
-            ""
-          else
-            "; charset=UTF-8")
+        (if (b.toString.toLowerCase.contains("charset="))
+          ""
+        else
+          "; charset=UTF-8")
       val postAction =
         new Runnable {
           def run() {
-            val conn = url.openConnection
-            conn.setConnectTimeout(10000)
-            conn.setReadTimeout(5000)
-            conn.setDoOutput(true)
-            conn.setRequestProperty("User-Agent", userAgent)
-            conn.setRequestProperty("Content-Type", postContentType)
-            conn.connect()
+            try {
+              val conn = url.openConnection
+              conn.setConnectTimeout(10000)
+              conn.setReadTimeout(5000)
+              conn.setDoOutput(true)
+              conn.setRequestProperty("User-Agent", userAgent)
+              conn.setRequestProperty("Content-Type", postContentType)
+              conn.connect()
 
-            val out = new OutputStreamWriter(conn.getOutputStream, charEncodingFromContentType(postContentType))
-            out.write(post)
-            out.close
+              val out = new OutputStreamWriter(conn.getOutputStream, charEncodingFromContentType(postContentType))
+              out.write(post)
+              out.close
 
-            val contentType = conn.getContentType()
-            //TODO: Confirm our assumption that the content is a character stream
-            val charEncoding = charEncodingFromContentType(contentType)
-            val in = Source.fromInputStream(conn.getInputStream, charEncoding)
-            val result = in.mkString
-            in.close
-            h.publish(result)
+              val contentType = conn.getContentType()
+              //TODO: Confirm our assumption that the content is a character stream
+              val charEncoding = charEncodingFromContentType(contentType)
+              val in = Source.fromInputStream(conn.getInputStream, charEncoding)
+              val result = in.mkString
+              in.close
+              h.publish(result)
+            } catch {
+              case ie: InterruptedException => { h.halt; throw ie }
+              case e: Exception => { h !! new JavaException(e) }
+            }
+
           }
         }
       (new Thread(postAction)).start()
