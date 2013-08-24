defb75dc301b509ff95485911779ee575db65145
John A. Thywissen <jthywiss@users.noreply.github.com>
2013-08-24 19:11:43 +0000

    Fix a Logger TODO, and create a logger for use in orc.lib.web

---
+++ OrcScala/src/orc/lib/web/Logger.scala
@@ -0,0 +1,21 @@
+//
+// Logger.scala -- Scala object Logger
+// Project OrcScala
+//
+// $Id$
+//
+// Created by jthywiss on Aug 24, 2013.
+//
+// Copyright (c) 2013 The University of Texas at Austin. All rights reserved.
+//
+// Use and redistribution of this file is governed by the license terms in
+// the LICENSE file found in the project's top-level directory and also found at
+// URL: http://orc.csres.utexas.edu/license.shtml .
+//
+package orc.lib.web
+
+/** Logger for the orc.lib.web subsystem
+  *
+  * @author jthywiss
+  */
+object Logger extends orc.util.Logger("orc.lib.web")
