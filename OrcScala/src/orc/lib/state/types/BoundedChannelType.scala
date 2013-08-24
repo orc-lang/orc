7dec455aa3443db406b39b6cae9667009dfe8d40
John A. Thywissen <jthywiss@users.noreply.github.com>
2013-08-24 23:26:18 +0000

    Minor typo fixes in orc.lib.state

--- OrcScala/src/orc/lib/state/types/BoundedChannelType.scala
+++ OrcScala/src/orc/lib/state/types/BoundedChannelType.scala
@@ -1,12 +1,12 @@
 //
-// ChannelType.scala -- Scala object ChannelType
+// BoundedChannelType.scala -- Scala object BoundedChannelType
 // Project OrcScala
 //
 // $Id$
 //
 // Created by dkitchin on Dec 1, 2010.
 //
-// Copyright (c) 2011 The University of Texas at Austin. All rights reserved.
+// Copyright (c) 2013 The University of Texas at Austin. All rights reserved.
 //
 // Use and redistribution of this file is governed by the license terms in
 // the LICENSE file found in the project's top-level directory and also found at
