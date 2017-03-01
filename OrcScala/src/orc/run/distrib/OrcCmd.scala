989bd07dfe738cc930ca673bdd897659010f6492
John A. Thywissen <jthywiss@users.noreply.github.com>
2017-02-28 22:36:29 -0600

    [minor] Add explicit empty c'tor args

--- OrcScala/src/orc/run/distrib/OrcCmd.scala
+++ OrcScala/src/orc/run/distrib/OrcCmd.scala
@@ -4,7 +4,7 @@
 //
 // Created by jthywiss on Dec 21, 2015.
 //
-// Copyright (c) 2016 The University of Texas at Austin. All rights reserved.
+// Copyright (c) 2017 The University of Texas at Austin. All rights reserved.
 //
 // Use and redistribution of this file is governed by the license terms in
 // the LICENSE file found in the project's top-level directory and also found at
@@ -21,7 +21,7 @@ import orc.{ OrcEvent, OrcExecutionOptions }
   *
   * @author jthywiss
   */
-sealed abstract class OrcCmd extends Serializable
+sealed abstract class OrcCmd() extends Serializable
 
 sealed trait OrcLeaderToFollowerCmd extends OrcCmd
 sealed trait OrcFollowerToLeaderCmd extends OrcCmd
