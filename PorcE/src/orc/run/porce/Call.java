bcbc2b5429c951b0f3c290910a56d47f462b152b
Arthur Peters <amp@singingwizard.org>
2017-08-15 11:14:44 -0500

    PorcE: Add interpreter level support for InvocationIntercepter.

--- PorcE/src/orc/run/porce/Call.java
+++ PorcE/src/orc/run/porce/Call.java
@@ -1,63 +1,112 @@
 
 package orc.run.porce;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.Node.Children;
 import com.oracle.truffle.api.profiles.ConditionProfile;
 import orc.run.porce.runtime.PorcEClosure;
 import orc.run.porce.runtime.PorcEExecutionRef;
 
-public class Call extends Expression {
-    @Child
-    protected CallBase internalCall;
-    @Child
-    protected CallBase externalCall;
-    @Child
-    protected Expression target;
+public abstract class Call extends CallBase {
+	@Child
+	protected CallBase internalCall = null;
+	@Child
+	protected CallBase externalCall = null;
+	@Child
+	protected InterceptedCall interceptedCall = null;
 
-    private final ConditionProfile profiler = ConditionProfile.createBinaryProfile();
+	private final ConditionProfile profileIsInternal = ConditionProfile.createBinaryProfile();
+	private final ConditionProfile profileIsIntercepted = ConditionProfile.createBinaryProfile();
 
-    public Call(final Expression target, final Expression[] arguments, final PorcEExecutionRef execution, final CallBase externalCall) {
-        this.target = (Expression) target.copy();
-        this.internalCall = InternalCall.create((Expression) target.copy(), CallBase.copyExpressionArray(arguments), execution);
-        this.externalCall = externalCall;
-    }
+	public Call(final Expression target, final Expression[] arguments, final PorcEExecutionRef execution) {
+		super(target, arguments, execution);
+	}
 
-    @Override
-    public Object execute(final VirtualFrame frame) {
-        // TODO: PERFORMANCE: This will execute the target an extra time for
-        // every call. If we could replace this call with the chain directly
-        // that would be better.
-        // That would require that all cache nodes support a common interface so
-        // that they can all call into each other. It should be easy actually.
+	protected CallBase makeInternalCall() {
+		return InternalCall.create((Expression) target.copy(), CallBase.copyExpressionArray(arguments), execution);
+	}
 
-        // TODO: PERFORMANCE: Even in the single type cache situation arguments
-        // and target get re-executed at each level. The optimizer might get rid
-        // of it, but that need to be verified.
+	protected CallBase getInternalCall() {
+		if (internalCall == null) {
+			CompilerDirectives.transferToInterpreterAndInvalidate();
+			this.internalCall = insert(makeInternalCall());
+		}
+		return internalCall;
+	}
 
-        // Fixing both above cases will be easiest by converting all the call code 
-        // to use the DSL (for caching and other things) and "execute..." methods 
-        // which the DSL can generate.
+	protected abstract CallBase makeExternalCall();
 
-        if (profiler.profile(isInternal(target.execute(frame)))) {
-            return internalCall.execute(frame);
-        } else {
-            return externalCall.execute(frame);
-        }
-    }
+	protected CallBase getExternalCall() {
+		if (externalCall == null) {
+			CompilerDirectives.transferToInterpreterAndInvalidate();
+			this.externalCall = insert(makeExternalCall());
+		}
+		return externalCall;
+	}
+
+	protected InterceptedCall makeInterceptedCall() {
+		return InterceptedCall.create((Expression) target.copy(), CallBase.copyExpressionArray(arguments), execution);
+	}
+
+	protected InterceptedCall getInterceptedCall() {
+		if (interceptedCall == null) {
+			CompilerDirectives.transferToInterpreterAndInvalidate();
+			this.interceptedCall = insert(makeInterceptedCall());
+		}
+		return interceptedCall;
+	}
+
+	@Override
+	public Object execute(final VirtualFrame frame) {
+		// TODO: PERFORMANCE: This will execute the target an extra time for
+		// every call. If we could replace this call with the chain directly
+		// that would be better.
+		// That would require that all cache nodes support a common interface so
+		// that they can all call into each other. It should be easy actually.
+
+		// TODO: PERFORMANCE: Even in the single type cache situation arguments
+		// and target get re-executed at each level. The optimizer might get rid
+		// of it, but that need to be verified.
+
+		// Fixing both above cases will be easiest by converting all the call code
+		// to use the DSL (for caching and other things) and "execute..." methods
+		// which the DSL can generate.
+		
+		final Object targetValue = executeTargetObject(frame);
+		final Object[] argumentValues = new Object[arguments.length];
+		executeArguments(argumentValues, 0, 0, frame);
+
+		if (profileIsIntercepted.profile(execution.get().shouldInterceptInvocation(targetValue, argumentValues))) {
+			return getInterceptedCall().executeWithValues(frame, targetValue, argumentValues);
+		} else if (profileIsInternal.profile(isInternal(targetValue))) {
+			return getInternalCall().execute(frame);
+		} else {
+			return getExternalCall().execute(frame);
+		}
+	}
 
     public static class Direct {
         public static Call create(final Expression target, final Expression[] arguments, final PorcEExecutionRef execution) {
-            return new Call(target, arguments, execution, ExternalDirectCall.create(target, arguments, execution));
-        }
-    }
+            return new Call(target, arguments, execution) {
+                protected CallBase makeExternalCall() {
+                       return ExternalDirectCall.create((Expression) target.copy(), CallBase.copyExpressionArray(arguments), execution);
+                }
+            };
+       }
+   }
 
-    public static class CPS {
-        public static Call create(final Expression target, final Expression[] arguments, final PorcEExecutionRef execution) {
-            return new Call(target, arguments, execution, ExternalCPSCall.create(target, arguments, execution));
+   public static class CPS {
+       public static Call create(final Expression target, final Expression[] arguments, final PorcEExecutionRef execution) {
+            return new Call(target, arguments, execution) {
+                protected CallBase makeExternalCall() {
+                       return ExternalCPSCall.create((Expression) target.copy(), CallBase.copyExpressionArray(arguments), execution);
+                }
+            };
         }
-    }
+	}
 
-    public static boolean isInternal(final Object t) {
-        return t instanceof PorcEClosure;
-    }
+	public static boolean isInternal(final Object t) {
+		return t instanceof PorcEClosure;
+	}
 }
---
+++ PorcE/src/orc/run/porce/InterceptedCall.java
@@ -0,0 +1,46 @@
+package orc.run.porce;
+
+import com.oracle.truffle.api.frame.VirtualFrame;
+
+import orc.run.porce.runtime.CPSCallResponseHandler;
+import orc.run.porce.runtime.Counter;
+import orc.run.porce.runtime.PorcEClosure;
+import orc.run.porce.runtime.PorcEExecutionRef;
+import orc.run.porce.runtime.Terminator;
+
+public class InterceptedCall extends CallBase {
+	protected InterceptedCall(Expression target, Expression[] arguments, PorcEExecutionRef execution) {
+		super(target, arguments, execution);
+	}
+
+	public Object executeWithValues(final VirtualFrame frame, final Object target, final Object[] arguments) {
+		final PorcEClosure pub = (PorcEClosure) arguments[0];
+		final Counter counter = (Counter) arguments[1];
+		final Terminator term = (Terminator) arguments[2];
+
+		// Token: Passed to handle from arguments.
+		final CPSCallResponseHandler handle = new CPSCallResponseHandler(execution.get(), pub, counter, term, getCallSiteId());
+
+		execution.get().invokeIntercepted(handle.callRecord(), target, buildArgumentValues(arguments));
+		return PorcEUnit.SINGLETON;
+	}
+
+	@Override
+	public Object execute(final VirtualFrame frame) {
+		final Object targetValue = executeTargetClosure(frame);
+		final Object[] argumentValues = new Object[arguments.length];
+		executeArguments(argumentValues, 0, 0, frame);
+
+		return executeWithValues(frame, targetValue, argumentValues);
+	}
+
+	protected Object[] buildArgumentValues(final Object[] arguments) {
+		final Object[] argumentValues = new Object[arguments.length - 3];
+		System.arraycopy(arguments, 3, argumentValues, 0, argumentValues.length);
+		return argumentValues;
+	}
+
+	public static InterceptedCall create(Expression target, Expression[] arguments, PorcEExecutionRef execution) {
+		return new InterceptedCall(target, arguments, execution);
+	}
+}
