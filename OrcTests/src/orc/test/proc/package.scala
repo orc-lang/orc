4d3bad494b3f98710d2cd2b9bc7003f2a66e8c11
John A. Thywissen <jthywiss@users.noreply.github.com>
2017-09-08 15:06:34 -0500

    Organize OrcTests into test items, procedures, and utilities.
    See package object comments for the distinctions.
    
    (Err, here is said package objects....)

---
+++ OrcTests/src/orc/test/proc/package.scala
@@ -0,0 +1,27 @@
+//
+// package.scala -- Scala package object for package orc.test.proc
+// Project project_name
+//
+// Created by jthywiss on Sep 8, 2017.
+//
+// Copyright (c) 2017 The University of Texas at Austin. All rights reserved.
+//
+// Use and redistribution of this file is governed by the license terms in
+// the LICENSE file found in the project's top-level directory and also found at
+// URL: http://orc.csres.utexas.edu/license.shtml .
+//
+
+package orc.test
+
+/** Test procedures and test cases for Orc testing.
+  *  
+  * A test procedure is a sequence of test cases in execution order,
+  * and any associated actions that may be required to set up the initial
+  * preconditions and any wrap up activities post execution.
+  * 
+  * A test case is a set of test case preconditions, inputs, and expected
+  * results, developed to drive the execution of a test item.
+  */
+package object proc {
+  
+}
