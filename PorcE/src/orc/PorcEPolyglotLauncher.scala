a8d490d31e373f67cdd3001d2f90c8349a535d4d
Arthur Peters <amp@singingwizard.org>
2017-09-25 21:23:49 -0500

    PorcE: Make Porc profiler easier to turn off and on.

--- PorcE/src/orc/PorcEPolyglotLauncher.scala
+++ PorcE/src/orc/PorcEPolyglotLauncher.scala
@@ -8,10 +8,6 @@ import orc.run.porce.PorcELanguage
 import com.oracle.truffle.api.source.Source
 import orc.script.OrcBindings
 import java.io.File
-//import orc.run.porce.instruments.PorcNodeExecutionProfiler
-//import orc.run.porce.instruments.PorcNodeExecutionProfilerInstrument
-//import java.util.Timer
-//import java.util.TimerTask
 
 object PorcEPolyglotLauncher {
   class OrcCmdLineOptions() extends OrcBindings() with CmdLineOptions
@@ -24,25 +20,28 @@ object PorcEPolyglotLauncher {
     orcOptions = Some(options)
     val runtime = PolyglotRuntime.newBuilder().setOut(System.out).setErr(System.err).build()
     val engine = PolyglotEngine.newBuilder().runtime(runtime).build()
-    //val insts = runtime.getInstruments()
-    //val langs = engine.getLanguages()
 
-    //println(insts.asScala)
-    //println(langs.asScala)
-    //println(options.filename)
-
-    /*
-    insts.get(PorcNodeExecutionProfilerInstrument.ID).setEnabled(true)
-    val profiler = PorcNodeExecutionProfiler.get(engine)
+    val profilePorcNodes = true
     
-    val timer = new Timer(true)
-    timer.scheduleAtFixedRate(new TimerTask {
-      def run(): Unit = {
-        profiler.dump()
-        profiler.reset()
-      }
-    }, 90 * 1000, 20 * 1000)
-    */
+    if (profilePorcNodes) {
+      import orc.run.porce.instruments.PorcNodeExecutionProfiler
+      import orc.run.porce.instruments.PorcNodeExecutionProfilerInstrument
+      import java.util.Timer
+      import java.util.TimerTask
+
+      val insts = runtime.getInstruments()
+
+      insts.get(PorcNodeExecutionProfilerInstrument.ID).setEnabled(true)
+      val profiler = PorcNodeExecutionProfiler.get(engine)
+      
+      val timer = new Timer(true)
+      timer.scheduleAtFixedRate(new TimerTask {
+        def run(): Unit = {
+          profiler.dump()
+          profiler.reset()
+        }
+      }, 90 * 1000, 20 * 1000)
+    }
 
     engine.eval(Source.newBuilder(new File(options.filename)).mimeType(PorcELanguage.MIME_TYPE).build())
     
