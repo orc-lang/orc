34cd655cde3a361b6cba1b2e851ef67098993ff9
John A. Thywissen <jthywiss@users.noreply.github.com>
2017-09-28 11:08:38 -0500

    Add test run number utilities

---
+++ OrcTests/src/orc/test/util/TestRunNumber.scala
@@ -0,0 +1,47 @@
+//
+// TestRunNumber.scala -- Scala object TestRunNumber
+// Project OrcTests
+//
+// Created by jthywiss on Sep 16, 2017.
+//
+// Copyright (c) 2017 The University of Texas at Austin. All rights reserved.
+//
+// Use and redistribution of this file is governed by the license terms in
+// the LICENSE file found in the project's top-level directory and also found at
+// URL: http://orc.csres.utexas.edu/license.shtml .
+//
+
+package orc.test.util
+
+/** Get a new run sequence number by invoking the get-new-run-seq-num.sh
+  * shell script.
+  *
+  * @author jthywiss
+  */
+object TestRunNumber {
+  def getNext(): String = {
+    val runCounterFilename = Config.get("testRunNumberFile")
+    /*FIXME: Need a better way to find the script.  This depends on details of our build procedure. */
+    val getNewRunNumCmdName = Config.get("testRunNumberCommandName").getOrElse("build/orc/test/util/get-new-run-seq-num.sh")
+    val getNewRunNumCmd = if (runCounterFilename.isDefined) Seq(getNewRunNumCmdName, runCounterFilename.get) else Seq(getNewRunNumCmdName)
+    val result = Config.get("testRunNumberServer") match {
+      case None => OsCommand.getResultFrom(getNewRunNumCmd)
+      case Some(hostname) => OsCommand.getResultFrom(Seq("ssh", hostname) ++ getNewRunNumCmd)
+    }
+    if (result.exitValue != 0) {
+      print(result.stdout)
+      Console.err.print(result.stderr)
+      throw new TestRunNumberCmdException(s"${getNewRunNumCmd.mkString(" ")} failed: exitValue=${result.exitValue}, stderr=${result.stderr}")
+    }
+    result.stdout
+  }
+  object Config extends orc.util.Config("TestRunNumberConfig.properties") { }
+
+  def main(args: Array[String]): Unit = {
+    println(getNext())
+  }
+}
+
+private class TestRunNumberCmdException(message: String, cause: Throwable) extends RuntimeException(message, cause) {
+  def this(message: String) = this(message, null)
+}
---
+++ OrcTests/src/orc/test/util/get-new-run-seq-num.sh
@@ -0,0 +1,61 @@
+#! /bin/sh
+
+# get-new-run-seq-num.sh -- Shell script to atomically get a new run sequence number
+# Project OrcTest
+#
+# Created by jthywiss on Sep 16, 2017.
+#
+# Copyright (c) 2017 The University of Texas at Austin. All rights reserved.
+#
+# Use and redistribution of this file is governed by the license terms in
+# the LICENSE file found in the project's top-level directory and also found at
+# URL: http://orc.csres.utexas.edu/license.shtml .
+#
+
+set -u
+set +C
+
+today=$(date "+%Y%m%d")
+# epoch year "a" = 2004, year of first Orc paper
+yearLetter=$(printf "\x$(printf %x $(( 97 + $(date -u +%Y) - 2004 )) )")
+dayOfYear=$(date "+%j")
+
+userInitial=$(printf "%.1s" "$(logname)")
+
+if [ $# -gt 1 ] ; then
+    echo "usage: $0 [run_counter_filename]" 1>&2
+    echo '    where run_counter_filename defaults to ${HOME}/.orc-test-run-counter' 1>&2
+    exit 64 # usage
+fi
+
+filename=${1:-${HOME}/.orc-test-run-counter}
+
+# Only symlink() is specified as atomic on both POSIX and NFSv3.
+# Note that shell noclobber and mkdir are NOT necessarily atomic.
+while ! ln -s "${filename}" "${filename}.lock" 2>/dev/null; do
+    sleep 1
+done
+
+trap "mv \"${filename}.lock\" \"${filename}.deleteme\" && rm -f \"${filename}.deleteme\"" 0
+
+if [ -e "${filename}" ]; then
+    read runDate runNum <"${filename}"
+    if [ "X${runDate}" = "X${today}" ]; then
+        runNum=$((runNum + 1))
+    else
+        runNum=1
+    fi
+else
+    runNum=1
+fi
+
+paddedRunNum=$(printf "%03d" "${runNum}")
+
+echo ${today} ${runNum} >"${filename}"
+
+mv "${filename}.lock" "${filename}.deleteme" && rm -f "${filename}.deleteme"
+
+trap - 0
+
+#echo "${yearLetter}${dayOfYear}${userInitial}${paddedRunNum}"
+echo "${today}-${userInitial}${paddedRunNum}"
