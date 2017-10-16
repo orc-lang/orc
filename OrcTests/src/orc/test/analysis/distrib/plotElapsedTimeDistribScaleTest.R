8a573a9aff66966820a3fac126bab18313276f3b
John A. Thywissen <jthywiss@users.noreply.github.com>
2017-10-16 11:20:06 -0500

    [WIP] Plot baseline times and speedup factors
    Well, attempt to plot the speedup.  Everything works except grabbing the
    baseline from the data.  Adding the speedup baseline column is dummied,
    so the plotted "speedup" is actually elapsed time.

--- OrcTests/src/orc/test/analysis/distrib/plotElapsedTimeDistribScaleTest.R
+++ OrcTests/src/orc/test/analysis/distrib/plotElapsedTimeDistribScaleTest.R
@@ -10,36 +10,58 @@
 # URL: http://orc.csres.utexas.edu/license.shtml .
 #
 
-library(plyr)
+library(dplyr)
 library(ggplot2)
 
-repetitionTimes <- read.csv("repetition-times.csv")
-names(repetitionTimes) <- c("program", "repeatRead", "numInputFiles", "dOrcNumRuntimes", "repetitionNumber", "elapsedTime")
+source("analysis.R")
 
-elapsedTimeSummary <- ddply(repetitionTimes, c("program", "repeatRead", "numInputFiles", "dOrcNumRuntimes"), summarise, nElapsedTime = length(elapsedTime), meanElapsedTime = mean(elapsedTime), sdElapsedTime = sd(elapsedTime), seElapsedTime = sdElapsedTime / sqrt(nElapsedTime))
+allRepetitionTimes <- read.csv("repetition-times.csv")
+names(allRepetitionTimes) <- c("program", "repeatRead", "numInputFiles", "dOrcNumRuntimes", "repetitionNumber", "elapsedTime")
 
+warmRepetitionTimes <- allRepetitionTimes[allRepetitionTimes$repetitionNumber >= 3,]
 
-for (currProgram in unique(elapsedTimeSummary$program)) {
-  if (currProgram != "WordCount.java") {
-    ggplot(subset(elapsedTimeSummary, program == currProgram), aes(x = factor(numInputFiles), y = meanElapsedTime, fill = factor(dOrcNumRuntimes), position = "dodge")) +
-        geom_col(position = "dodge") +
-        ggtitle(currProgram) +
-        xlab("Number of files read") +
-        labs(fill = "Cluster size [Number of d-Orc runtimes]") +
-        scale_y_continuous(name = "Elapsed time (s)", labels = function(n){format(n / 1000000, scientific = FALSE)}) +
-    #    geom_errorbar(aes(ymax = meanElapsedTime + seElapsedTime, ymin = meanElapsedTime - seElapsedTime), position = "dodge") +
-        theme_minimal() +
-        theme(legend.justification = c(0, 1), legend.position = c(0, 1))
-  } else {
-    ggplot(subset(elapsedTimeSummary, program == currProgram), aes(x = factor(numInputFiles), y = meanElapsedTime, position = "dodge")) +
-        geom_col(position = "dodge") +
-        ggtitle(currProgram) +
-        xlab("Number of files read") +
-        scale_y_continuous(name = "Elapsed time (s)", labels = function(n){format(n / 1000000, scientific = FALSE)}) +
-    #    geom_errorbar(aes(ymax = meanElapsedTime + seElapsedTime, ymin = meanElapsedTime - seElapsedTime), position = "dodge") +
-        theme_minimal() +
-        theme(legend.justification = c(0, 1), legend.position = c(0, 1))
-  }
+baselineTimes <- warmRepetitionTimes[warmRepetitionTimes$dOrcNumRuntimes == 1 | is.na(warmRepetitionTimes$dOrcNumRuntimes),]
 
-    ggsave(paste0("elapsedTime_", currProgram, ".pdf"))
+# Plot baseline elapsed times
+
+for (currProgram in unique(baselineTimes$program)) {
+  ggplot(baselineTimes[baselineTimes$program == currProgram,], aes(x = factor(numInputFiles), y = elapsedTime, group = 1)) +
+  geom_point() +
+  stat_summary(fun.y = mean, geom = "line") +
+  ggtitle(currProgram) +
+  xlab("Number of files read") +
+  labs(fill = "Cluster size [Number of d-Orc runtimes]") +
+  scale_y_continuous(name = "Elapsed time (s)", labels = function(n){format(n / 1000000, scientific = FALSE)}) +
+  #    geom_errorbar(aes(ymax = meanElapsedTime + seElapsedTime, ymin = meanElapsedTime - seElapsedTime), position = "dodge") +
+  theme_minimal() +
+  theme(legend.justification = c(0, 1), legend.position = c(0, 1))
+
+  ggsave(paste0("elapsedTime_", currProgram, ".pdf"), width = 7, height = 7)
+}
+
+# Plot speedups
+
+elapsedTimeSummary <- warmRepetitionTimes[warmRepetitionTimes$repetitionNumber >= 3,] %>%
+  group_by(program, repeatRead, numInputFiles, dOrcNumRuntimes) %>%
+  summarise(nElapsedTime = length(elapsedTime), meanElapsedTime = mean(elapsedTime), sdElapsedTime = sd(elapsedTime), seElapsedTime = sdElapsedTime / sqrt(nElapsedTime))
+
+#Broken:
+# elapsedTimeSummary <- elapsedTimeSummary %>% addBaseline(vars(meanElapsedTime), c(dOrcNumRuntimes = 1))
+elapsedTimeSummary <- elapsedTimeSummary %>% mutate(meanElapsedTime_baseline = 1.0)
+
+for (currProgram in unique(elapsedTimeSummary$program[elapsedTimeSummary$dOrcNumRuntimes > 1 & !is.na(elapsedTimeSummary$dOrcNumRuntimes)])) {
+  baseline <- baselineTimes[baselineTimes$program == currProgram,]
+
+  ggplot(elapsedTimeSummary[elapsedTimeSummary$program == currProgram & elapsedTimeSummary$dOrcNumRuntimes > 1,], aes(x = factor(numInputFiles), y = meanElapsedTime / meanElapsedTime_baseline, group = factor(dOrcNumRuntimes), colour = factor(dOrcNumRuntimes), shape = factor(dOrcNumRuntimes))) +
+  geom_line() +
+  geom_point(size = 2) +
+  ggtitle(currProgram) +
+  xlab("Number of files read") +
+  labs(colour = "Cluster size [Number of d-Orc runtimes]", shape = "Cluster size [Number of d-Orc runtimes]") +
+  scale_y_continuous(name = "Speed-up factor over cluster size 1", labels = function(n){format(n / 1000000, scientific = FALSE)}) +
+  geom_errorbar(aes(ymax = meanElapsedTime + seElapsedTime, ymin = meanElapsedTime - seElapsedTime), width = 0.2, alpha = 0.35) +
+  theme_minimal() +
+  theme(legend.justification = c(0, 1), legend.position = c(0, 1))
+
+  ggsave(paste0("speedup_", currProgram, ".pdf"), width = 7, height = 7)
 }
