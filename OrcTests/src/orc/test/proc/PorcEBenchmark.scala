27aefc149bff65a4baaca0685e86dc2598208a0e
Arthur Peters <amp@singingwizard.org>
2017-10-17 20:54:19 -0500

    Rework benchmark harness to run Scala tests as well as Orc.

--- OrcTests/src/orc/test/proc/PorcEBenchmark.scala
+++ OrcTests/src/orc/test/proc/PorcEBenchmark.scala
@@ -21,81 +21,18 @@ import orc.test.util.ExperimentalCondition
 import orc.test.util.FactorValue
 import orc.test.util.RemoteCommand
 
-trait PorcEExperimentalCondition extends ExperimentalCondition {
-  override def toJvmArgs: Iterable[String] = {
-    val factorProps = factorDescriptions.zipWithIndex.flatMap({ case (fd, i) => 
-      val v = productElement(i).asInstanceOf[AnyRef] 
-      val base = s"${FactorValue.factorPropertyPrefix}${fd.id}"
-      Map(
-          s"$base" -> v,
-          s"$base.name" -> fd.name,
-          s"$base.comments" -> fd.comments,
-          s"$base.unit" -> fd.unit,
-          )
-    })
-    val sysProps = systemProperties ++ factorProps
-    val sysPropsAsArgs = for ((k, v) <- sysProps) yield s"-D$k=$v"
-    sysPropsAsArgs
-  }
-  def toOrcArgs: Iterable[String] = Seq()
-    
-  def systemProperties: Map[String, Any] = Map()
-  
-  def toFilePrefix = orcFile.getName.stripSuffix(".orc") + "_" + productIterator.filterNot(v => v.asInstanceOf[AnyRef] eq orcFile).mkString("_")
-  
-  val orcFile: File
-  
-  def wrapperCmd: Seq[String] = Seq()
-}
-
-trait PorcEBenchmark {
+// FIXME: This object contains a total mess. But I wanted to at least slightly abstract what I'm doing below. Needs to be refactored.
+object ArthursBenchmarkEnv {
   val workingDir = System.getProperty("user.dir", ".")
   val testRootDir = new File(workingDir).getParent
   val targetHost = Option(System.getProperty("orc.test.benchmarkingHost"))
-  val remoteJavaHome = System.getProperty("orc.test.remoteJavaHome", "LocalInstalls/graalvm-0.28.2/jre")
   val targetBinariesDir = if (targetHost.isDefined) "orc-binaries" else testRootDir
-  val remoteOutputDir = "runs"
-  
-  val filesToCopy = Seq(
-      s"OrcScala/build",
-      s"OrcScala/lib",
-      s"PorcE/build/classes",
-      s"OrcTests/build",
-      s"OrcTests/test_data"
-      )
-  
-  val classPathSeq = Seq(
-      s"$targetBinariesDir/OrcScala/build/*",
-      s"$targetBinariesDir/OrcScala/lib/*",
-      s"$targetBinariesDir/PorcE/build/classes",
-      s"$targetBinariesDir/OrcTests/build",
-      )
-      
+        
   def makePathRemotable(p: String) = {
     targetBinariesDir + "/" + p.stripPrefix(testRootDir).stripPrefix("/")
   }
-  
-  def quoteForShell(p: String) = {
-    s"'$p'"
-  }
 
-  val bootPath = {
-    val jh = if (targetHost.isDefined) remoteJavaHome else System.getProperty("java.home")
-    s"${jh}/lib/truffle/truffle-api.jar"
-  }
-  val classPath = classPathSeq.mkString(":")
-
-  val javaCmd = if (targetHost.isDefined) {
-    remoteJavaHome + File.separator + "bin" + File.separator + "java";
-  } else if (System.getProperty("os.name").startsWith("Win")) {
-    System.getProperty("java.home") + File.separator + "bin" + File.separator + "java.exe";
-  } else {
-    System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
-  }
-
-  val problemSize = System.getProperty("orc.test.benchmark.problemSize", "1").toInt
-  val nRuns = System.getProperty("orc.test.benchmark.nRuns", "50").toInt
-    
+      
   /** This is a list of the all the CPU ids in the order they should be assigned.
     * 
     */
@@ -106,71 +43,170 @@ trait PorcEBenchmark {
   
   def tasksetCommandPrefix(nCPUs: Int) = Seq("taskset", "--cpu-list", cpuIDList.take(nCPUs).mkString(","))
 
-  protected def run(
-    expCondition: PorcEExperimentalCondition,
-    //jvmOptions: Seq[String],
-    //orcOptions: Seq[String],
-    //systemProperties: Map[String, String],
-    runOutputDir: String) = {
-    new File(runOutputDir).mkdirs()
-    targetHost.foreach(RemoteCommand.mkdir(_, remoteOutputDir))
-
-    val outFilenamePrefix = s"${expCondition.toFilePrefix}"
-    //val systemPropOptions = for ((k, v) <- systemProperties) yield s"-D$k=$v"
-    val outFile = s"${runOutputDir}/${outFilenamePrefix}_0.out"
-    val errFile = s"${runOutputDir}/${outFilenamePrefix}_0.err"
+  trait JVMRunner {
+    def remoteJavaHome: String
     
-    val outputDir = if (targetHost.isDefined) remoteOutputDir else runOutputDir
+    val problemSize = System.getProperty("orc.test.benchmark.problemSize", "1").toInt
+    val nRuns = System.getProperty("orc.test.benchmark.nRuns", "50").toInt
+  
+    val remoteOutputDir = "runs"
     
-    val sshCommand = targetHost.toSeq.flatMap(host => Seq("ssh", host))
+    val filesToCopy = Seq(
+        s"OrcScala/build",
+        s"OrcScala/lib",
+        s"PorcE/build/classes",
+        s"OrcTests/build",
+        s"OrcTests/test_data"
+        )
     
-    val commandLine = expCondition.wrapperCmd ++
-        Seq(javaCmd, "-XX:-UseJVMCIClassLoader", "-XX:-UseJVMCICompiler", "-cp", classPath, s"-Xbootclasspath/a:$bootPath") ++
-        //jvmOptions ++
-        //systemPropOptions ++
-        Seq(
-          s"-Dorc.test.benchmark.datadir=$targetBinariesDir/OrcTests/",
-          s"-Dorc.test.benchmark.problemSize=${problemSize}",
-          s"-Dorc.test.benchmark.nRuns=${nRuns}",
-          s"-Dorc.executionlog.fileprefix=${outFilenamePrefix}_",
-          "-Dorc.executionlog.filesuffix=_0",
-          "-Dorc.config.dirs=config",
-          s"-Dorc.executionlog.dir=${outputDir}",
-          // Always trace compilation since it will be useful for determining where outlyers come from. 
-          "-Dgraal.TraceTruffleCompilation=true", 
-          "-Dgraal.TruffleBackgroundCompilation=false",
-          s"-Dgraal.TruffleCompilerThreads=8") ++
-        expCondition.toJvmArgs ++
-        Seq("orc.Main",
-          "--backend", "porc",
-          "-O", "3") ++
-        //orcOptions ++
-        expCondition.toOrcArgs ++
-        Seq(makePathRemotable(expCondition.orcFile.getPath))
-
-    println(s"\n==== Starting $expCondition ====")
-    println(s"With command line:\n${commandLine.mkString("\n  ")}")
+    val classPathSeq = Seq(
+        s"$targetBinariesDir/OrcScala/build/*",
+        s"$targetBinariesDir/OrcScala/lib/*",
+        s"$targetBinariesDir/PorcE/build/classes",
+        s"$targetBinariesDir/OrcTests/build",
+        )
     
-    val maybeQuotedCommandLine = if (targetHost.isDefined) commandLine.map(quoteForShell) else commandLine
+    def quoteForShell(p: String) = {
+      s"'$p'"
+    }
+  
+    val bootPath = {
+      val jh = if (targetHost.isDefined) remoteJavaHome else System.getProperty("java.home")
+      s"${jh}/lib/truffle/truffle-api.jar"
+    }
+    val classPath = classPathSeq.mkString(":")
+  
+    val javaCmd = if (targetHost.isDefined) {
+      remoteJavaHome + File.separator + "bin" + File.separator + "java";
+    } else if (System.getProperty("os.name").startsWith("Win")) {
+      System.getProperty("java.home") + File.separator + "bin" + File.separator + "java.exe";
+    } else {
+      System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+    }
+  
+    protected def runJVM(
+      expCondition: JVMExperimentalCondition,
+      //jvmOptions: Seq[String],
+      //orcOptions: Seq[String],
+      //systemProperties: Map[String, String],
+      runOutputDir: String) = {
+      new File(runOutputDir).mkdirs()
+      targetHost.foreach(RemoteCommand.mkdir(_, remoteOutputDir))
+  
+      val outFilenamePrefix = s"${expCondition.toFilePrefix}"
+      //val systemPropOptions = for ((k, v) <- systemProperties) yield s"-D$k=$v"
+      val outFile = s"${runOutputDir}/${outFilenamePrefix}_0.out"
+      val errFile = s"${runOutputDir}/${outFilenamePrefix}_0.err"
       
-    
-    OsCommand.getResultFrom(
-      sshCommand ++ maybeQuotedCommandLine,
-      directory = new File(workingDir),
-      teeStdOutErr = true,
-      stdoutTee = Seq(System.out, new FileOutputStream(outFile)),
-      stderrTee = Seq(System.err, new FileOutputStream(errFile)))
-    
-    targetHost match {
-      case Some(host) => 
-        RemoteCommand.rsyncFromRemote(host, s"$remoteOutputDir/", runOutputDir)
-        RemoteCommand.deleteDirectory(host, s"$remoteOutputDir")
-      case None => {
+      val outputDir = if (targetHost.isDefined) remoteOutputDir else runOutputDir
+      
+      val sshCommand = targetHost.toSeq.flatMap(host => Seq("ssh", host))
+      
+      val commandLine = expCondition.wrapperCmd ++
+          Seq(javaCmd, "-cp", classPath, s"-Xbootclasspath/a:$bootPath") ++
+          //jvmOptions ++
+          //systemPropOptions ++
+          Seq(
+            s"-Dorc.test.benchmark.datadir=$targetBinariesDir/OrcTests/",
+            s"-Dorc.test.benchmark.problemSize=${problemSize}",
+            s"-Dorc.test.benchmark.nRuns=${nRuns}",
+            s"-Dorc.executionlog.fileprefix=${outFilenamePrefix}_",
+            "-Dorc.executionlog.filesuffix=_0",
+            "-Dorc.config.dirs=config",
+            s"-Dorc.executionlog.dir=${outputDir}") ++
+          expCondition.toJvmArgs
+  
+      println(s"\n==== Starting $expCondition ====")
+      println(s"With command line:\n${commandLine.mkString(" ")}")
+      
+      val maybeQuotedCommandLine = if (targetHost.isDefined) commandLine.map(quoteForShell) else commandLine
+      
+      OsCommand.getResultFrom(
+        sshCommand ++ maybeQuotedCommandLine,
+        directory = new File(workingDir),
+        teeStdOutErr = true,
+        stdoutTee = Seq(System.out, new FileOutputStream(outFile)),
+        stderrTee = Seq(System.err, new FileOutputStream(errFile)))
+      
+      targetHost match {
+        case Some(host) => 
+          RemoteCommand.rsyncFromRemote(host, s"$remoteOutputDir/", runOutputDir)
+          RemoteCommand.deleteDirectory(host, s"$remoteOutputDir")
+        case None => {
+        }
       }
     }
   }
+
   
-  def runExperiment(experimentalConditions: Iterable[PorcEExperimentalCondition]) = {
+  trait JVMExperimentalCondition extends ExperimentalCondition {
+    override def toJvmArgs: Iterable[String] = {
+      val factorProps = factorDescriptions.zipWithIndex.flatMap({ case (fd, i) => 
+        val v = productElement(i).asInstanceOf[AnyRef] 
+        val base = s"${FactorValue.factorPropertyPrefix}${fd.id}"
+        Map(
+            s"$base" -> v,
+            s"$base.name" -> fd.name,
+            s"$base.comments" -> fd.comments,
+            s"$base.unit" -> fd.unit,
+            )
+      })
+      val sysProps = systemProperties ++ factorProps
+      val sysPropsAsArgs = for ((k, v) <- sysProps) yield s"-D$k=$v"
+      sysPropsAsArgs
+    }
+      
+    def systemProperties: Map[String, Any] = Map()
+    
+    def toFilePrefix: String
+    
+    def wrapperCmd: Seq[String] = Seq()
+  }
+
+  trait PorcEExperimentalCondition extends JVMExperimentalCondition {
+    override def toJvmArgs = super.toJvmArgs ++
+          Seq("-XX:-UseJVMCIClassLoader", "-XX:-UseJVMCICompiler",
+            // Always trace compilation since it will be useful for determining where outlyers come from. 
+            "-Dgraal.TraceTruffleCompilation=true", 
+            "-Dgraal.TruffleBackgroundCompilation=false",
+            s"-Dgraal.TruffleCompilerThreads=8") ++
+          Seq("orc.Main",
+            "--backend", "porc",
+            "-O", "3") ++
+          toOrcArgs ++
+          Seq(makePathRemotable(orcFile.getPath))
+
+    def toOrcArgs: Iterable[String] = Seq()
+    
+    def toFilePrefix = orcFile.getName.stripSuffix(".orc") + "_" + productIterator.filterNot(v => v.asInstanceOf[AnyRef] eq orcFile).mkString("_")
+    
+    val orcFile: File
+  }
+
+  trait ScalaExperimentalCondition extends JVMExperimentalCondition {
+    override def toJvmArgs = super.toJvmArgs ++
+          Seq(benchmarkClass.getCanonicalName,
+            "--benchmark")
+          
+    def toFilePrefix = benchmarkClass.getSimpleName.stripSuffix("$") + "_" + productIterator.filterNot(v => v.asInstanceOf[AnyRef] eq benchmarkClass).mkString("_")
+    
+    val benchmarkClass: Class[_]
+  }
+  
+  trait CPUControlExperimentalCondition extends JVMExperimentalCondition {
+    val nCPUs: Int
+    
+    override def wrapperCmd = tasksetCommandPrefix(nCPUs)
+  }
+  
+}
+
+import ArthursBenchmarkEnv._
+
+trait PorcEBenchmark extends JVMRunner {
+  lazy val remoteJavaHome = System.getProperty("orc.test.remoteJavaHome", "LocalInstalls/graalvm-0.28.2/jre")
+  
+  def runExperiment(experimentalConditions: Iterable[JVMExperimentalCondition]) = {
     println(s"Running experiments:\n  ${experimentalConditions.mkString("\n  ")}")
     ExperimentalCondition.writeExperimentalConditionsTable(experimentalConditions)
     
@@ -186,7 +222,7 @@ trait PorcEBenchmark {
     
     val runOutputDir = System.getProperty("orc.executionlog.dir")
     for (expCondition <- experimentalConditions) {
-      run(expCondition, runOutputDir)
+      runJVM(expCondition, runOutputDir)
     }
   }
 }
--- OrcTests/src/orc/test/proc/porcEBenchmarkExperiments.scala
+++ OrcTests/src/orc/test/proc/porcEBenchmarkExperiments.scala
@@ -5,37 +5,61 @@ import java.io.File
 
 object PorcEStrongScalingExperiment extends PorcEBenchmark {
     
-  val factors = Seq(
-    FactorDescription("orcFile", "Orc File", "", "The Orc program file name"),
-    FactorDescription("nCPUs", "Number of CPUs", "", "The number of CPUs to use"),
-    FactorDescription("TruffleBackgroundCompilation", "Truffle Background Compilation", "", "Should truffle compile in the background"),
-    FactorDescription("allowSpawnInlining", "Allow Spawn Inlining", "", ""),
-    FactorDescription("optLevel", "Optimization Level", "", "-O level")
-  )
-
-  case class MyExperimentalCondition(orcFile: File, nCPUs: Int, truffleBackgroundCompilation: Boolean, allowSpawnInlining: Boolean, optLevel: Int) extends PorcEExperimentalCondition {
-    override def factorDescriptions = factors
+  case class MyPorcEExperimentalCondition(
+      orcFile: File, 
+      nCPUs: Int, 
+      truffleBackgroundCompilation: Boolean, 
+      allowSpawnInlining: Boolean, 
+      optLevel: Int) 
+      extends ArthursBenchmarkEnv.PorcEExperimentalCondition with ArthursBenchmarkEnv.CPUControlExperimentalCondition {
+    override def factorDescriptions = Seq(
+      FactorDescription("orcFile", "Orc File", "", "The Orc program file name"),
+      FactorDescription("nCPUs", "Number of CPUs", "", "The number of CPUs to use"),
+      FactorDescription("TruffleBackgroundCompilation", "Truffle Background Compilation", "", "Should truffle compile in the background"),
+      FactorDescription("allowSpawnInlining", "Allow Spawn Inlining", "", ""),
+      FactorDescription("optLevel", "Optimization Level", "", "-O level")
+    )
     override def systemProperties = super.systemProperties ++ Map(
         "graal.TruffleBackgroundCompilation" -> truffleBackgroundCompilation,
         "orc.porce.allowSpawnInlining" -> allowSpawnInlining
         )
         
     override def toOrcArgs = super.toOrcArgs ++ Seq("-O", optLevel.toString)
-    
-    override def wrapperCmd = tasksetCommandPrefix(nCPUs)
+  }
+  case class MyScalaExperimentalCondition(
+      benchmarkClass: Class[_], 
+      nCPUs: Int) 
+      extends ArthursBenchmarkEnv.ScalaExperimentalCondition with ArthursBenchmarkEnv.CPUControlExperimentalCondition {
+    override def factorDescriptions = Seq(
+      FactorDescription("benchmarkClass", "Benchmark Class", "", "The class run for this benchmark"),
+      FactorDescription("nCPUs", "Number of CPUs", "", "The number of CPUs to use")
+    )
   }
 
   def main(args: Array[String]): Unit = {
     val experimentalConditions = {
-      for {
+      val porce = for {
         optLevel <- Seq(3)
         nCPUs <- (Seq(1) ++ (4 to 24 by 4) ++ Seq(32, 48)).reverse 
         allowSpawnInlining <- Seq(true)
         fn <- args.toSeq
       } yield {
         assert(new File(fn).isFile())
-        MyExperimentalCondition(new File(fn), nCPUs, false, allowSpawnInlining, optLevel)
+        MyPorcEExperimentalCondition(new File(fn), nCPUs, false, allowSpawnInlining, optLevel)
       }
+      val scala = for {
+        nCPUs <- (Seq(1) ++ (4 to 24 by 4) ++ Seq(32, 48)).reverse 
+        clsName <- Seq(
+            "orc.test.item.scalabenchmarks.Mandelbrot",
+            "orc.test.item.scalabenchmarks.swaptions.SwaptionsParTrial", 
+            "orc.test.item.scalabenchmarks.swaptions.SwaptionsParSwaption"
+            )
+      } yield {
+        val cls = Class.forName(clsName)
+        assert(cls != null)
+        MyScalaExperimentalCondition(cls, nCPUs)
+      }
+      scala ++ porce
     }
     runExperiment(experimentalConditions)
   }
