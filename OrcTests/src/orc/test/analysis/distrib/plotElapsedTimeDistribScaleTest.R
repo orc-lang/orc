e712c1eb18dbd8cfb361300cd6e0c30befa91842
John A. Thywissen <jthywiss@users.noreply.github.com>
2017-10-31 07:10:42 -0500

    Chg speed-up x-axis to cluster size; warm-ups=10 reps; Add run num title

--- OrcTests/src/orc/test/analysis/distrib/plotElapsedTimeDistribScaleTest.R
+++ OrcTests/src/orc/test/analysis/distrib/plotElapsedTimeDistribScaleTest.R
@@ -15,10 +15,12 @@ library(ggplot2)
 
 source("analysis.R")
 
+runNumber <- commandArgs(trailingOnly = TRUE)[1]
+
 allRepetitionTimes <- read.csv("repetition-times.csv")
 names(allRepetitionTimes) <- c("program", "repeatRead", "numInputFiles", "dOrcNumRuntimes", "repetitionNumber", "elapsedTime", "cpuTime")
 
-warmRepetitionTimes <- allRepetitionTimes[allRepetitionTimes$repetitionNumber >= 3,]
+warmRepetitionTimes <- allRepetitionTimes[allRepetitionTimes$repetitionNumber >= 10,]
 
 baselineTimeSummary <- warmRepetitionTimes[warmRepetitionTimes$dOrcNumRuntimes == 1 | is.na(warmRepetitionTimes$dOrcNumRuntimes),] %>%
   group_by(program, repeatRead, numInputFiles, dOrcNumRuntimes) %>%
@@ -31,10 +33,11 @@ for (currProgram in unique(baselineTimeSummary$program)) {
   ggplot(baselineTimeSummary[baselineTimeSummary$program == currProgram,], aes(x = factor(numInputFiles), y = meanElapsedTime, group = 1)) +
   geom_point() +
   stat_summary(fun.y = mean, geom = "line") +
-  ggtitle(currProgram) +
+  ggtitle(paste(currProgram, "Run", runNumber)) +
   xlab("Number of files read") +
   labs(fill = "Cluster size [Number of d-Orc runtimes]") +
   scale_y_continuous(name = "Elapsed time (s)", labels = function(n){format(n / 1000000, scientific = FALSE)}) +
+  expand_limits(y = 0.0) +
   geom_errorbar(aes(ymax = meanElapsedTime + seElapsedTime, ymin = meanElapsedTime - seElapsedTime), width = 0.2, alpha = 0.35, position = "dodge") +
   theme_minimal() +
   theme(legend.justification = c(0, 1), legend.position = c(0, 1))
@@ -55,13 +58,14 @@ options(tibble.print_max = Inf, tibble.width = Inf)
 
 for (currProgram in unique(elapsedTimeSummary$program[elapsedTimeSummary$dOrcNumRuntimes > 1 & !is.na(elapsedTimeSummary$dOrcNumRuntimes)])) {
 
-  ggplot(elapsedTimeSummary[elapsedTimeSummary$program == currProgram & elapsedTimeSummary$dOrcNumRuntimes > 1,], aes(x = factor(numInputFiles), y = speedup, group = factor(dOrcNumRuntimes), colour = factor(dOrcNumRuntimes), shape = factor(dOrcNumRuntimes))) +
+  ggplot(elapsedTimeSummary[elapsedTimeSummary$program == currProgram & elapsedTimeSummary$dOrcNumRuntimes > 1,], aes(x = factor(dOrcNumRuntimes), y = speedup, group = factor(numInputFiles), colour = factor(numInputFiles), shape = factor(numInputFiles))) +
   geom_line() +
   geom_point(size = 3) +
-  ggtitle(currProgram) +
-  xlab("Number of files read") +
-  labs(colour = "Cluster size [Number of d-Orc runtimes]", shape = "Cluster size [Number of d-Orc runtimes]") +
+  ggtitle(paste(currProgram, "Run", runNumber)) +
+  xlab("Cluster size [Number of d-Orc runtimes]") +
+  labs(colour = "Number of files read", shape = "Number of files read") +
   scale_y_continuous(name = "Speed-up factor over cluster size 1", labels = function(n){format(n, scientific = FALSE)}) +
+  expand_limits(y = 1.0) +
   # geom_errorbar(aes(ymax = speedupSeMax, ymin = speedupSeMin), width = 0.2, alpha = 0.35) +
   theme_minimal() +
   theme(legend.justification = c(0, 1), legend.position = c(0, 1))
