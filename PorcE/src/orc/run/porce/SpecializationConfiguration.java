acd09d3221c8aaed38c104b8faec14e4fe36eea0
Arthur Peters <amp@singingwizard.org>
2017-11-02 11:58:52 -0500

    PorcE: Make specialization of calls into Orc much more aggressive.
    This may not help things in all cases, but it allows important
    optimizations between multiple functions in the same TCO
    trampoline.

--- PorcE/src/orc/run/porce/SpecializationConfiguration.java
+++ PorcE/src/orc/run/porce/SpecializationConfiguration.java
@@ -6,12 +6,14 @@ import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 public abstract class SpecializationConfiguration {
 	@CompilationFinal
     public static final int GetFieldMaxCacheSize = 4;
+	
 	@CompilationFinal
-    public static final int InternalCallMaxCacheSize = 12;
+    public static final int InternalCallMaxCacheSize = 50;
 	@CompilationFinal
     public static final int ExternalDirectCallMaxCacheSize = 4;
 	@CompilationFinal
     public static final int ExternalCPSCallMaxCacheSize = 4;
+	
 	@CompilationFinal
     public static final int InlineAverageTimeLimit = 1 * 1000000; // In ns
 	@CompilationFinal
