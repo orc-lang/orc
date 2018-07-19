333f5978fc9d49c206da25df1f5a0fc902284493
John A. Thywissen <jthywiss@users.noreply.github.com>
2018-07-19 14:44:57 -0500

    Make SemaphoreInstance a nested class and make its c'tor public

--- OrcScala/src/orc/lib/state/Semaphore.java
+++ OrcScala/src/orc/lib/state/Semaphore.java
@@ -55,7 +55,7 @@ public class Semaphore extends EvalSite implements TypedSite {
         return SemaphoreType.getBuilder();
     }
 
-    public class SemaphoreInstance extends DotSite implements DOrcPlacementPolicy {
+    public static class SemaphoreInstance extends DotSite implements DOrcPlacementPolicy {
 
         protected final LinkedList<CallContext> waiters = new LinkedList<CallContext>();
         protected final LinkedList<CallContext> snoopers = new LinkedList<CallContext>();
@@ -64,7 +64,7 @@ public class Semaphore extends EvalSite implements TypedSite {
         protected int n;
 
         /* Precondition: n >= 0 */
-        SemaphoreInstance(final int n) {
+        public SemaphoreInstance(final int n) {
             this.n = n;
         }
 
