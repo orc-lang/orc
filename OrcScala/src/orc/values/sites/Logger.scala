5342596ba41a6286b3a68a68b629f68f4932a63d
John A. Thywissen <jthywiss@users.noreply.github.com>
2018-10-15 11:47:55 -0500

    Create a Logger for the sites package. Uncomment some logging.

---
+++ OrcScala/src/orc/values/sites/Logger.scala
@@ -0,0 +1,20 @@
+//
+// Logger.scala -- Scala object Logger
+// Project project_name
+//
+// Created by jthywiss on Oct 15, 2018.
+//
+// Copyright (c) 2018 The University of Texas at Austin. All rights reserved.
+//
+// Use and redistribution of this file is governed by the license terms in
+// the LICENSE file found in the project's top-level directory and also found at
+// URL: http://orc.csres.utexas.edu/license.shtml .
+//
+
+package orc.values.sites
+
+/** Logger for the orc.values.sites package
+  *
+  * @author jthywiss
+  */
+object Logger extends orc.util.Logger("orc.values.sites")
