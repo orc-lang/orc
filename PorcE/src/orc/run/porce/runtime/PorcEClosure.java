ac678bd343eede1b67ac31cc4579064bed7b43a3
John A. Thywissen <jthywiss@users.noreply.github.com>
2018-10-26 10:48:34 -0500

    Reflectively initialize PorcEClosure fields during deserializatio

--- PorcE/src/orc/run/porce/runtime/PorcEClosure.java
+++ PorcE/src/orc/run/porce/runtime/PorcEClosure.java
@@ -11,7 +11,14 @@
 
 package orc.run.porce.runtime;
 
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.ObjectInputStream;
+import java.io.ObjectInputValidation;
+import java.io.ObjectOutputStream;
+import java.io.ObjectStreamField;
 import java.io.Serializable;
+import java.lang.reflect.Field;
 
 import orc.run.porce.PorcERootNode;
 import orc.values.Format;
@@ -22,22 +29,36 @@ import com.oracle.truffle.api.nodes.RootNode;
 import com.oracle.truffle.api.profiles.ValueProfile;
 
 /**
- * A class representing PorcE closures including both Orc methods (def, site) and internal Porc continuations.
- *
+ * A class representing PorcE closures including both Orc methods (def, site)
+ * and internal Porc continuations.
+ * <p>
+ * Serialization: PorcEClosure relies on an external helper during serialization
+ * and deserialization to translate instance state to/from a Serializable
+ * marshaledFieldData. This is the only field written to the I/O stream.
+ * 
  * @author amp
  */
-final public class PorcEClosure implements Serializable {
+final public class PorcEClosure implements Serializable, ObjectInputValidation {
     private static final long serialVersionUID = 3075892152696469588L;
-    //These public fields were final, but have been un-final-ed so deserialization can initialize them.
-    //TODO: Make these fields final again, and cram values in using reflection in marshalingInitFieldData.
-    public transient Object[] environment;
-    public transient RootCallTarget body;
-    private Object marshaledFieldData; 
+    /**
+     * @serialField marshaledFieldData Serializable
+     */
+    private static final ObjectStreamField[] serialPersistentFields = { new ObjectStreamField("marshaledFieldData", Serializable.class) };
+    private Serializable marshaledFieldData;
+
+    /*
+     * NOTE! If fields are added/removed, update our PorcEClosure-custom
+     * serialization logic.
+     */
+
+    public final Object[] environment;
+    public final RootCallTarget body;
 
     public final boolean isRoutine;
 
-    // TODO: PERFORMANCE: Using a frame instead of an array for captured values may perform better. Though that will
-    // mainly be true when we start using native values.
+    // TODO: PERFORMANCE: Using a frame instead of an array for captured values
+    // may perform better. Though that will mainly be true when we start using
+    // native values.
     public PorcEClosure(final Object[] environment, final RootCallTarget body, final boolean isRoutine) {
         CompilerDirectives.interpreterOnly(() -> {
             if (body == null) {
@@ -55,17 +76,17 @@ final public class PorcEClosure implements Serializable {
 
     public long getTimePerCall() {
         if (body.getRootNode() instanceof PorcERootNode) {
-            PorcERootNode root = (PorcERootNode) body.getRootNode();
+            final PorcERootNode root = (PorcERootNode) body.getRootNode();
             return root.getTimePerCall();
         } else {
             return Long.MAX_VALUE;
         }
     }
 
-    public long getTimePerCall(ValueProfile bodyProfile) {
-        RootNode r = bodyProfile.profile(body.getRootNode());
+    public long getTimePerCall(final ValueProfile bodyProfile) {
+        final RootNode r = bodyProfile.profile(body.getRootNode());
         if (r instanceof PorcERootNode) {
-            PorcERootNode root = (PorcERootNode) r;
+            final PorcERootNode root = (PorcERootNode) r;
             return root.getTimePerCall();
         } else {
             return Long.MAX_VALUE;
@@ -81,7 +102,7 @@ final public class PorcEClosure implements Serializable {
         return body.call((Object) environment);
     }
 
-    /*
+    /*-
     public Object callFromRuntime(final Object p1) {
         return body.call(environment, p1);
     }
@@ -102,18 +123,52 @@ final public class PorcEClosure implements Serializable {
     }
     */
 
-    public Object getMarshaledFieldData() {
+    public Serializable getMarshaledFieldData() {
         return marshaledFieldData;
     }
 
-    public void setMarshaledFieldData(Object marshaledFieldData) {
+    public void setMarshaledFieldData(final Serializable marshaledFieldData) {
         this.marshaledFieldData = marshaledFieldData;
     }
 
+    private void writeObject(final ObjectOutputStream out) throws IOException {
+        if (marshaledFieldData == null) {
+            throw new InvalidObjectException("Attempt to serialize PorcEClosure without having set marshaledFieldData");
+        }
+        out.defaultWriteObject();
+    }
+
+    private void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        in.registerValidation(this, 0);
+    }
+
+    @Override
+    public void validateObject() throws InvalidObjectException {
+        if (body == null) {
+            throw new InvalidObjectException("Attempt to deserialize PorcEClosure without having initialized body from marshaledFieldData");
+        }
+        if (environment == null) {
+            throw new InvalidObjectException("Attempt to deserialize PorcEClosure without having initialized environment from marshaledFieldData");
+        }
+    }
+
     public void marshalingInitFieldData(final Object[] newEnvironment, final RootCallTarget newBody) {
-        assert environment == null && body == null : "marshalingInitFieldData on already initialized PorcEClosure" ;
-        this.environment = newEnvironment;
-        this.body = newBody;
+        if (environment != null || body != null) {
+            throw new IllegalStateException("marshalingInitFieldData on already initialized PorcEClosure");
+        }
+
+        /* Use reflection to cram unmarshaled initial values into final fields */
+        try {
+            final Field environmentField = this.getClass().getField("environment");
+            final Field bodyField = this.getClass().getField("body");
+            environmentField.setAccessible(true);
+            bodyField.setAccessible(true);
+            environmentField.set(this, newEnvironment);
+            bodyField.set(this, newBody);
+        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+            throw new AssertionError("Failed to reflectively initialize PorcEClosure 'environment' or 'body' field during deserialization", e);
+        }
     }
 
     private static final ThreadLocal<Boolean> toStringRecursionCheck = new ThreadLocal<Boolean>() {
@@ -127,16 +182,17 @@ final public class PorcEClosure implements Serializable {
     @Override
     @SuppressWarnings("boxing")
     public String toString() {
-        // FIXME: The recursion check will be SLOW. But it may not matter. However it may not even be good to have this
+        // FIXME: The recursion check will be SLOW. But it may not matter.
+        // However it may not even be good to have this
         // as a default.
-        StringBuilder sb = new StringBuilder();
-        RootNode rootNode = body.getRootNode();
+        final StringBuilder sb = new StringBuilder();
+        final RootNode rootNode = body.getRootNode();
         sb.append(rootNode.getName());
-        Class<? extends RootNode> rootNodeClass = rootNode.getClass();
+        final Class<? extends RootNode> rootNodeClass = rootNode.getClass();
         scala.collection.Iterator<orc.ast.porc.Variable> closureVariablesIter = null;
         if (rootNode instanceof PorcERootNode) {
-            PorcERootNode r = (PorcERootNode) rootNode;
-            closureVariablesIter = ((scala.collection.IterableLike<orc.ast.porc.Variable, ?>)r.getClosureVariables()).toIterator();
+            final PorcERootNode r = (PorcERootNode) rootNode;
+            closureVariablesIter = ((scala.collection.IterableLike<orc.ast.porc.Variable, ?>) r.getClosureVariables()).toIterator();
         } else {
             sb.append(':');
             sb.append(rootNodeClass.getSimpleName());
@@ -145,16 +201,16 @@ final public class PorcEClosure implements Serializable {
             toStringRecursionCheck.set(true);
             sb.append('[');
             boolean notFirst = false;
-            for (Object v : environment) {
+            for (final Object v : environment) {
                 if (notFirst) {
                     sb.append(", ");
                 }
                 if (closureVariablesIter != null && closureVariablesIter.hasNext()) {
-                    orc.ast.porc.Variable var = closureVariablesIter.next();
+                    final orc.ast.porc.Variable var = closureVariablesIter.next();
                     sb.append(var);
                     sb.append("=");
                 }
-                String s = Format.formatValue(v);
+                final String s = Format.formatValue(v);
                 sb.append(s.substring(0, s.length() < 50 ? s.length() : 49));
                 notFirst = true;
             }
