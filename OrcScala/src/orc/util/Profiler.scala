3a17d3fa0dade1f5fbbd2d54586fd66ba83da710
John A. Thywissen <jthywiss@users.noreply.github.com>
2019-02-28 12:52:50 -0600

    Add statistics for collected profile intervals

--- OrcScala/src/orc/util/Profiler.scala
+++ OrcScala/src/orc/util/Profiler.scala
@@ -4,7 +4,7 @@
 //
 // Created by jthywiss on Mar 26, 2017.
 //
-// Copyright (c) 2017 The University of Texas at Austin. All rights reserved.
+// Copyright (c) 2019 The University of Texas at Austin. All rights reserved.
 //
 // Use and redistribution of this file is governed by the license terms in
 // the LICENSE file found in the project's top-level directory and also found at
@@ -39,7 +39,8 @@ object Profiler {
 
   private final val profilingAccumulatorsTL = if (profilerOn) new ThreadLocal[ProfilingAccumulators]() {
     override protected def initialValue = new ProfilingAccumulators(Thread.currentThread().getId)
-  } else null
+  }
+  else null
 
   type IntervalBeginning = Long
 
@@ -81,10 +82,28 @@ object Profiler {
       if (accums.isDefined) {
         accums.get(0) += intervalCount
         accums.get(1) += intervalDurationNanos
+        /* duration ^ 2 can overflow a Long, so switch to milliseconds in that case */
+        val oldDurn2 = accums.get(2)
+        if (oldDurn2 >= 0 && (oldDurn2 + intervalDurationNanos * intervalDurationNanos) >= 0) {
+          accums.get(2) += intervalDurationNanos * intervalDurationNanos
+        } else {
+          if (oldDurn2 >= 0) {
+            /* Just overflowed, switch to millisecond mode */
+            accums.get(2) = -(accums.get(2) / 1000000000000L)
+          }
+          /* Now in millisecond mode */
+          val intervalDurationMillis = intervalDurationNanos / 1000000L
+          accums.get(2) -= intervalDurationMillis * intervalDurationMillis
+        }
+        accums.get(3) = Math.min(accums.get(3), intervalDurationNanos)
+        accums.get(4) = Math.max(accums.get(4), intervalDurationNanos)
       } else {
-        val newAccums = new Array[Long](2)
+        val newAccums = new Array[Long](5)
         newAccums(0) = intervalCount
         newAccums(1) = intervalDurationNanos
+        newAccums(2) = intervalDurationNanos * intervalDurationNanos
+        newAccums(3) = intervalDurationNanos
+        newAccums(4) = intervalDurationNanos
         accumulatorMap.put(intervalType, newAccums)
       }
     }
@@ -100,9 +119,12 @@ object Profiler {
 
       for (pa <- accums) {
         for (e <- pa.accumulatorMap) {
-          val sums = sumMap.getOrElseUpdate(e._1, Array(0L, 0L))
+          val sums = sumMap.getOrElseUpdate(e._1, Array(0L, 0L, 0L, Long.MaxValue, -1L))
           sums(0) += e._2(0)
           sums(1) += e._2(1)
+          sums(2) += e._2(2)
+          sums(3) = Math.min(sums(3), e._2(3))
+          sums(4) = Math.max(sums(4), e._2(4))
           if (e._1.name.length > intervalTypeColWidth) intervalTypeColWidth = e._1.name.length
         }
       }
@@ -111,11 +133,11 @@ object Profiler {
       System.err synchronized {
         System.err.append(s"Profiling Accumulators: begin, ${sumMap.size} entries\n")
         System.err.append("Interval-Type".padTo(intervalTypeColWidth, '-'))
-        System.err.append("\t-------Count--------\t--Accum.-Time-(ns)--\n")
+        System.err.append("\t-------Count--------\t--Accum.-Time-(ns)--\tAccum.-Time^2-(ns^2)\t---Min.-Time-(ns)---\t---Max.-Time-(ns)---\n")
 
         for (e <- sumMap) {
           System.err.append(e._1.name.padTo(intervalTypeColWidth, ' '))
-          System.err.append(f"\t${e._2(0)}%20d\t${e._2(1)}%20d\n")
+          System.err.append(f"\t${e._2(0)}%20d\t${e._2(1)}%20d\t${if (e._2(2) >= 0) e._2(2).toDouble else (e._2(2).toDouble * -1e12)}%20.15g\t${e._2(3)}%20d\t${e._2(4)}%20d\n")
         }
         System.err.append(f"Profiling Accumulators: end\n")
       }
@@ -124,9 +146,9 @@ object Profiler {
       if (csvOut.isDefined) {
         val profileCsv = new OutputStreamWriter(csvOut.get, "UTF-8")
         val csvWriter = new CsvWriter(profileCsv.append(_))
-        val tableColumnTitles = Seq("Interval Type", "Count", "Accum. Time (ns)")
+        val tableColumnTitles = Seq("Interval Type", "Count", "Accum. Time (ns)", "Accum. Time^2 (ns^2)", "Min. Time (ns)", "Max. Time (ns)")
         csvWriter.writeHeader(tableColumnTitles)
-        val rows = sumMap.map(e => ((e._1.name,e._2(0),e._2(1))))
+        val rows = sumMap.map(e => ((e._1.name, e._2(0), e._2(1), if (e._2(2) >= 0) e._2(2).toDouble else (e._2(2).toDouble * -1e12), e._2(3), e._2(4))))
         csvWriter.writeRows(rows)
         profileCsv.close()
       }
